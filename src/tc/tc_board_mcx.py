bb3a2aef6bb906ee55d66c3101e4a43ab4f892dc
Heiko Schocher <hs@denx.de>
2015-10-20 13:42:11 +0200

    tc, board mcx: updates
    - add pinmux checks
    - test linux stable and linux ml
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/tc_board_mcx.py
+++ src/tc/tc_board_mcx.py
@@ -13,46 +13,14 @@
 #
 # start with
 # python2.7 src/common/tbot.py -c tbot_mcx.cfg -t tc_board_mcx.py
-# start all testcases for the mcx board
+# start all testcases for the mcx board linux stable and linux-ml
 #
 from tbotlib import tbot
 
-#set board state for which the tc is valid
-tb.set_board_state("u-boot")
+tb.statusprint("tc_mcx testing linux stable")
+tb.eof_call_tc("tc_board_mcx_tests.py")
 
-tb.statusprint("u-boot setenv")
-#call ubot setenv
-tb.eof_call_tc("tc_ub_setenv.py")
-
-tb.statusprint("tc_shc linux dmesg checks")
-tb.tc_lx_dmesg_grep_name = "htkw mcx"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-
-tb.tc_lx_dmesg_grep_name = "Manufacturer ID: 0x2c"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "ti_hecc 5c050000.hecc: device registered"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "ehci-omap 48064800.ehci"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "usb usb1: New USB device"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "usb usb2: New USB device found"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "rtc-ds1307 0-0068: setting system"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "input: EP0700M06"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "Initialized drm"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "Initialized omapdrm"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-tb.tc_lx_dmesg_grep_name = "asix 2-1.3:1.0 eth1: register"
-tb.eof_call_tc("tc_lx_dmesg_grep.py")
-
-#call linux tc_lx_partition_check.py
-tb.eof_call_tc("tc_lx_partition_check.py")
-tb.eof_call_tc("tc_lx_bonnie.py")
-
-# power off board at the end
-tb.eof_call_tc("tc_lab_poweroff.py")
+tb.statusprint("tc_mcx testing linux mainline")
+tb.ub_boot_linux_cmd = 'run tbot_boot_linux_ml'
+tb.eof_call_tc("tc_board_mcx_tests.py")
 tb.end_tc(True)
---
+++ src/tc/tc_board_mcx_tests.py
@@ -0,0 +1,61 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# start with
+# python2.7 src/common/tbot.py -c tbot_mcx.cfg -t tc_board_mcx_tests.py
+# start all testcases for the mcx board
+#
+from tbotlib import tbot
+
+#set board state for which the tc is valid
+tb.set_board_state("u-boot")
+
+tb.statusprint("u-boot setenv")
+#call ubot setenv
+tb.eof_call_tc("tc_ub_setenv.py")
+
+tb.statusprint("tc_shc linux dmesg checks")
+checks = ['htkw mcx',
+'Linux version',
+'Kernel command line',
+'Manufacturer ID: 0x2c',
+'ti_hecc 5c050000.hecc: device registered',
+'ehci-omap 48064800.ehci',
+'usb usb1: New USB device',
+'usb usb2: New USB device found',
+'rtc-ds1307 0-0068: setting system',
+'input: EP0700M06',
+'Initialized drm',
+'Initialized omapdrm',
+'asix 2-1.3:1.0 eth1: register']
+
+for tb.tc_lx_dmesg_grep_name in checks:
+    tb.eof_call_tc("tc_lx_dmesg_grep.py")
+
+tb.statusprint("tc_mcx pinmux part 1 check")
+files = ['src/files/mcx_pinmux_part1.reg',
+'src/files/mcx_pinmux_part2.reg',
+'src/files/mcx_pinmux_part3.reg',
+'src/files/mcx_pinmux_part4.reg',
+'src/files/mcx_pinmux_part5.reg',
+'src/files/mcx_pinmux_part6.reg']
+for tb.tc_lx_create_reg_file_name in files:
+    tb.eof_call_tc("tc_lx_check_reg_file.py")
+
+#call linux tc_lx_partition_check.py
+tb.eof_call_tc("tc_lx_partition_check.py")
+tb.eof_call_tc("tc_lx_bonnie.py")
+
+# power off board at the end
+tb.eof_call_tc("tc_lab_poweroff.py")
+tb.end_tc(True)
