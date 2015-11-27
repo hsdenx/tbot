534f5270c2c730c84b47da9934e4c9dbcbe5850e
Heiko Schocher <hs@denx.de>
2015-11-27 10:35:58 +0100

    tc_ub_upd_uboot.py if uboot update fail, try it retry times
    try retry times an u-boot update.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/tc_ub_upd_uboot.py
+++ src/tc/tc_ub_upd_uboot.py
@@ -35,8 +35,33 @@ tb.eof_call_tc("tc_ub_load_board_env.py")
 # OK ...
 tb.eof_write_con("print tbot_upd_uboot")
 tb.eof_read_end_state_con(1)
-tb.eof_write_con("run tbot_upd_uboot")
-tb.eof_search_str_in_readline_con("OK")
+upd_fail = True
+i = 0
+retry = 2
+while upd_fail == True:
+    if i >= retry:
+        # board now maybe without u-boot
+        # try a save u-boot ?
+        tb.end_tc(False)
+
+    tb.eof_write_con("run tbot_upd_uboot")
+    searchlist = ["!= byte at", "error", "Retry count exceeded"]
+    tmp = True
+    upd_fail = False
+    while tmp == True:
+        tmp = tb.readline_and_search_strings(tb.channel_con, searchlist)
+        if tmp == 0:
+            upd_fail = True
+            tmp = True
+        elif tmp == 1:
+            upd_fail = True
+            tmp = True
+        elif tmp == 2:
+            upd_fail = True
+            tmp = True
+        elif tmp == 'prompt':
+            i += 1
+            tmp = False
 
 # "run tbot_cmp_uboot"
 tb.eof_write_con("print tbot_cmp_uboot")
