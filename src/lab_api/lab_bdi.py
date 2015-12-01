61b7700ab1dd1f14810650213cac3407fd30c9a3
Heiko Schocher <hs@denx.de>
2015-12-01 07:21:50 +0000

    lab_bdi: bad bdi hack
    BDI is a little bit tricky. I did not understand
    when to send a "\n" or a "\r\n" at the end, also
    always the "\n" comes in a new ssh packet ... :-(
    and even more worser, this leads into coming a BDI prompt
    ???
    doing here a big bad workaround it ...
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/lab_api/lab_bdi.py
+++ src/lab_api/lab_bdi.py
@@ -23,30 +23,64 @@ import time
 sys.path.append("./common")
 from tbotlib import tbot
 
+# BDI is a little bit tricky. I did not understand
+# when to send a "\n" or a "\r\n" at the end, also
+# always the "\n" comes in a new ssh packet ... :-(
+# and more worser, this leads into coming a BDI prompt
+# ???
+# doing here a big bad workaround it ...
+
 class bdi_class(object):
     def __init__(self, tb):
         self.tb = tb
 
     def get_bdi_prompt(self):
         ret = self.tb.read_end(self.tb.channel_ctrl, 1, self.tb.lab_bdi_upd_uboot_bdi_prompt)
-        if ret != True:
-            self.tb.end_tc(False)
+        #if ret != True:
+        #    self.tb.end_tc(False)
+
+    def read_return_and_prompt(self):
+        ret = True
+        while ret:
+            ret = self.tb.read_end(self.tb.channel_ctrl, 1, '\n')
+            # and we get a prompt
+            self.get_bdi_prompt()
 
     def send_bdi_cmd_wait_string_and_prompt(self, cmd, string):
-        self.tb.eof_write_ctrl(cmd + '\r\n')
+        self.tb.lab.write_no_ret(self.tb.channel_ctrl, cmd + '\r')
+        ret = self.tb.read_end(self.tb.channel_ctrl, 1, cmd)
         ret = self.tb.read_end(self.tb.channel_ctrl, 4, string)
         if ret != True:
             self.tb.end_tc(False)
-        self.get_bdi_prompt()
+        self.read_return_and_prompt()
 
     def send_bdi_cmd_wait_prompt(self, cmd):
-        self.tb.eof_write_ctrl(cmd + '\r\n')
+        self.tb.lab.write_no_ret(self.tb.channel_ctrl, cmd + '\r')
+        # wait command repeat
+        ret = self.tb.read_end(self.tb.channel_ctrl, 1, cmd)
+        self.read_return_and_prompt()
+
+    def bdi_connect(self):
+        i = 0
+        while i < 3:
+            self.tb.lab.write_no_ret(self.tb.channel_ctrl, self.tb.lab_bdi_upd_uboot_bdi_cmd + '\n')
+            ret = self.tb.read_end(self.tb.channel_ctrl, 1, self.tb.lab_bdi_upd_uboot_bdi_prompt)
+            if ret != True:
+                #maybe powered off, power on
+                self.tb.set_power_state('on')
+                i += 1
+            else:
+                i = 5
+
+    def bdi_quit(self):
+        self.tb.eof_write_ctrl("quit\r\n")
+        self.tb.eof_read_end_state_ctrl(3)
         self.get_bdi_prompt()
 
     def bdi_connect(self):
         i = 0
         while i < 3:
-            self.tb.eof_write_ctrl(self.tb.lab_bdi_upd_uboot_bdi_cmd)
+            self.tb.lab.write_no_ret(self.tb.channel_ctrl, self.tb.lab_bdi_upd_uboot_bdi_cmd + '\n')
             ret = self.tb.read_end(self.tb.channel_ctrl, 1, self.tb.lab_bdi_upd_uboot_bdi_prompt)
             if ret != True:
                 #maybe powered off, power on
