5dcdd07b12361a6d9caf55879adadd1b1243131b
Heiko Schocher <hs@denx.de>
2015-12-09 08:21:08 +0100

    helper script reformat_logfile.sh
    pass fd as first argument to it, as we now have not only
    console fd.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- reformat_logfile.sh
+++ reformat_logfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #delete all unnecessary line
-sed '/read 1/!d' $1 > gnlmpf
+sed '/read '$1'/!d' $2 > gnlmpf
 #delete ":tbotlib   # read_line hs@pollux.denx.org:"
-sed 's/INFO   :tbotlib   # read 1:/ /g' gnlmpf > $2
+sed 's/INFO   :tbotlib   # read '$1':/ /g' gnlmpf > $3
 rm gnlmpf
