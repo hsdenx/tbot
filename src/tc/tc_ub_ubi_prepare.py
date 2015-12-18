b5a6da227a73dd50bcc3594afb34e0a15ed93240
Heiko Schocher <hs@denx.de>
2015-12-18 08:21:52 +0100

    tc_ub_ubi_prepare.py: rework
    - first load always u-boot environment
    - check if ubi is attached, if so, detach it
    - call "ubi part..." and "ubi dettach"
      3 times.
      - to be sure more than one "ubi part" works
      - ubi fastmap gets written through such a
        cycle ... this is a bug in U-Boot
        Fix this ...
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/tc_ub_ubi_prepare.py
+++ src/tc/tc_ub_ubi_prepare.py
@@ -21,11 +21,77 @@ logging.info("args: %s %s", tb.tc_ub_ubi_prep_partname, tb.tc_ub_ubi_prep_offset
 #set board state for which the tc is valid
 tb.set_board_state("u-boot")
 
+#load ub tbot environment
+tb.eof_call_tc("tc_ub_load_board_env.py")
+
+# "ubi part" if yes -> call ubi dettach
+tmp = "if ubi part; then; echo OK; else; echo FAIL; fi"
+tb.eof_write_con(tmp)
+searchlist = ["OK"]
+tmp = True
+attached = False
+while tmp == True:
+    tmp = tb.readline_and_search_strings(tb.channel_con, searchlist)
+    if tmp == 0:
+        attached = True
+        tmp = True
+    elif tmp == 'prompt':
+        tmp = False
+
+if attached == True:
+    tb.eof_write_con("ubi dettach")
+    tb.eof_read_end_state_con(1)
+
 tmp = "ubi part " + tb.tc_ub_ubi_prep_partname
 if tb.tc_ub_ubi_prep_offset != 'none':
     tmp += " " + tb.tc_ub_ubi_prep_offset
 
-tb.eof_write_con(tmp)
-tb.eof_search_str_in_readline_con("available PEBs")
+def ubiprep(tb, tmp):
+    tb.eof_write_con(tmp)
+
+    searchlist = ["init error", "available PEBs", "empty MTD device detected"]
+    tmp = True
+    cmd_ok = False
+    i = 0
+    retry = 50
+    while i < retry:
+        tmp = tb.readline_and_search_strings(tb.channel_con, searchlist)
+        if tmp == None:
+            i += 1
+        elif tmp == 1:
+            i = 0
+            cmd_ok = True
+        elif tmp == 2:
+            i = 0
+        elif tmp == 'prompt':
+            i = retry
+        else:
+            i = 0
+
+    if i >= retry:
+        logging.info("%d >= %d cmd ok: %s", i, retry, cmd_ok)
+    if cmd_ok == False:
+        tb.end_tc(False)
+    return cmd_ok
+
+# we have two execute ubi part more than once,
+# -> ubi fastmap gets written ...
+# This is currently a bug in U-Boot ...
+cmd_ok=ubiprep(tb, tmp)
+#detach it
+tb.eof_write_con("ubi dettach")
+tb.eof_read_end_state_con(1)
+cmd_ok=ubiprep(tb, tmp)
+#detach it
+tb.eof_write_con("ubi dettach")
+tb.eof_read_end_state_con(1)
+cmd_ok=ubiprep(tb, tmp)
+
+#clear ubi part from uboot cmd buffer
+tb.eof_write_con("ubi info")
+tb.eof_read_end_state_con(1)
+
+if cmd_ok == False:
+    tb.end_tc(False)
 
 tb.end_tc(True)
