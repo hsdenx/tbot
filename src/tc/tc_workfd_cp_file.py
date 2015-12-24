c25789d3b868ed8363dde7d148f6d741c508e1ed
Heiko Schocher <hs@denx.de>
2015-12-24 06:33:48 +0100

    new tc tc_workfd_cp_file.py: simple copy files
    copy file tb.tc_workfd_cp_file_a to tb.tc_workfd_cp_file_b.
    No variables.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/tc_workfd_cp_file.py
@@ -0,0 +1,26 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# start with
+# python2.7 src/common/tbot.py -c tbot.cfg -t tc_workfd_cp_file.py
+# simple copy  file from a to b
+#
+from tbotlib import tbot
+
+logging.info("args: workfd %s %s %s", tb.workfd, tb.tc_workfd_cp_file_a, tb.tc_workfd_cp_file_b)
+
+tmp = "cp " + tb.tc_workfd_cp_file_a + " " + tb.tc_workfd_cp_file_b
+tb.eof_write(tb.workfd, tmp)
+tb.eof_read_end_state(tb.workfd, 1)
+tb.eof_call_tc("tc_workfd_check_cmd_success.py")
+tb.end_tc(True)
