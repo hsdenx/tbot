6f65faaee29364393d7ea86b203a89053296cc14
Heiko Schocher <hs@denx.de>
2016-05-03 12:39:14 +0200

    tc: tc_workfd_disconnect_with_kermit.py
    disconnect from a kermit connect
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/linux/tc_workfd_disconnect_with_kermit.py
@@ -0,0 +1,45 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# start with
+# python2.7 src/common/tbot.py -c tbot.cfg -t tc_workfd_connect_with_kermit.py
+# disconnect from a kermit connection
+from tbotlib import tbot
+
+logging.info("args: workdfd: %s", tb.workfd)
+logging.info("args: ssh: %s", tb.tc_workfd_connect_with_kermit_ssh)
+logging.info("args: kermit: %s %s", tb.kermit_line, tb.kermit_speed)
+
+string = pack('h', 28)
+string = string[:1]
+tb.workfd.send_raw(string)
+tb.workfd.send_raw('C')
+tb.workfd.set_prompt('C-Kermit>')
+tb.workfd.expect_prompt()
+
+# set lab pc linux prompt
+tb.workfd.set_prompt(tb.linux_prompt)
+tb.eof_write(tb.workfd, 'exit')
+searchlist = ['OK']
+tmp = True
+once = 0
+while tmp == True:
+    ret = tb.tbot_read_line_and_check_strings(tb.workfd, searchlist)
+    if ret == '0':
+        if once == 0:
+            tb.eof_write(tb.workfd, 'OK')
+            once = 1
+    if ret == 'prompt':
+        tmp = False
+
+tb.end_tc(True)
