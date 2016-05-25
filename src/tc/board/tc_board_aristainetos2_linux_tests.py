2c0324d82d2cc107ecd188e1552c3d56e976a2f5
Heiko Schocher <hs@denx.de>
2016-05-25 06:50:32 +0200

    board: aristainetos: add register checking
    check a lot of SoC registers
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/board/tc_board_aristainetos2_linux_tests.py
+++ src/tc/board/tc_board_aristainetos2_linux_tests.py
@@ -79,8 +79,6 @@ checks = ['Machine model: aristainetos2',
 for tb.tc_lx_dmesg_grep_name in checks:
     tb.eof_call_tc("tc_lx_dmesg_grep.py")
 
-tb.end_tc(True)
-
 tb.statusprint("tc_mcx pinmux check")
 files = ['src/files/aristainetos2_pinmux.reg']
 for tb.tc_lx_create_reg_file_name in files:
