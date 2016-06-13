8f27a09c0495e5a918e234466302ecc1d3e2161c
Heiko Schocher <hs@denx.de>
2016-06-13 19:47:08 +0200

    new tc tc_workfd_apply_patches.py
    apply patches with "patch" on current workfd connection.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/tc_workfd_apply_patches.py
@@ -0,0 +1,53 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# start with
+# python2.7 src/common/tbot.py -c tbot.cfg -t tc_lab_apply_patches.py
+# apply patches to source
+from tbotlib import tbot
+
+logging.info("args: %s", tb.tc_lab_apply_patches_dir)
+
+if tb.tc_lab_apply_patches_dir == 'none':
+    tb.end_tc(True)
+
+c = tb.workfd
+# apply all patches in tc_lab_apply_patches_dir
+tb.set_term_length(c)
+
+tmp = 'for i in ' + tb.tc_lab_apply_patches_dir + '/*.patch; do patch -p1 < $i; done'
+tb.eof_write(c, tmp)
+
+searchlist = ["No such", "FAILED", "Assume -R?"]
+tmp = True
+apply_ok = True
+while tmp == True:
+    tmp = tb.tbot_read_line_and_check_strings(c, searchlist)
+    if tmp == '0':
+        apply_ok = False
+        tmp = True
+    if tmp == '1':
+        apply_ok = False
+        tmp = True
+    if tmp == '2':
+        tb.eof_write(c, 'y')
+        tmp = True
+    elif tmp == 'prompt':
+        tmp = False
+
+if apply_ok == False:
+    tb.end_tc(False)
+
+tb.eof_call_tc("tc_workfd_check_cmd_success.py")
+
+tb.end_tc(True)
