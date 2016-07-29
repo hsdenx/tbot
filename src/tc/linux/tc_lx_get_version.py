af74dd1f47616af6c36b22a2874558c091090147
Heiko Schocher <hs@denx.de>
2016-07-29 16:06:15 +0200

    new tc: get linux version
    get current linux version, also create a new event
    LINUX_VERSION with the linux version value.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/linux/tc_lx_get_version.py
@@ -0,0 +1,37 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# start with
+# python2.7 src/common/tbot.py -c tbot.cfg -t tc_lx_get_version.py
+# get the linux version
+from tbotlib import tbot
+
+logging.info("args: %s", tb.workfd.name)
+
+#set board state for which the tc is valid
+tb.set_board_state("linux")
+
+c = tb.workfd
+tmp = 'cat /proc/version'
+tb.eof_write(c, tmp)
+ret = tb.tbot_expect_string(c, 'Linux version')
+if ret == 'prompt':
+    tb.end_tc(False)
+ret = tb.tbot_expect_string(c, '\n')
+if ret == 'prompt':
+    tb.end_tc(False)
+tb.tc_return = tb.buf.rstrip()
+tb.event.create_event('main', tb.boardname, "LINUX_VERSION", tb.tc_return)
+tb.tbot_expect_prompt(c)
+
+tb.end_tc(True)
