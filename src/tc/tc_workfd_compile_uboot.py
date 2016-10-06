7c9adfd6b3bc2395296fc493558dbdc760ca6b64
Heiko Schocher <hs@denx.de>
2016-10-06 08:30:36 +0200

    tc, tc_workfd_compile_uboot: set PATH only if not already set
    set PATH only if not already set.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/tc_workfd_compile_uboot.py
+++ src/tc/tc_workfd_compile_uboot.py
@@ -20,8 +20,11 @@
 from tbotlib import tbot
 
 if tb.tc_lab_compile_uboot_export_path != 'none':
-    tmp = "export PATH=" + tb.tc_lab_compile_uboot_export_path + ":$PATH"
-    tb.eof_write_lx_cmd_check(tb.workfd, tmp)
+    tmp = "printenv PATH | grep --color=never " + tb.tc_lab_compile_uboot_export_path
+    ret = tb.write_lx_cmd_check(tb.workfd, tmp)
+    if ret == False:
+        tmp = "export PATH=" + tb.tc_lab_compile_uboot_export_path + ":$PATH"
+        ret = tb.eof_write_lx_cmd_check(tb.workfd, tmp)
 
 tmp = "make mrproper"
 tb.eof_write_lx_cmd_check(tb.workfd, tmp)
