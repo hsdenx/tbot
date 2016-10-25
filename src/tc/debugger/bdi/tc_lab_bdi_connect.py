f10c26f982bb6c1ed9d1c06f728cd7314840a513
Heiko Schocher <hs@denx.de>
2016-10-25 17:33:09 +0200

    debugger, bdi: catch "No route" as error
    if we cannot reach the BDI wwith telnet, we get the string
    "No route". In this case, we cannot connect to BDI.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/debugger/bdi/tc_lab_bdi_connect.py
+++ src/tc/debugger/bdi/tc_lab_bdi_connect.py
@@ -31,11 +31,11 @@ if tb.config.board_has_debugger == 0:
 c = tb.workfd
 tb.write_stream(c, tb.config.lab_bdi_upd_uboot_bdi_cmd)
 c.set_prompt(tb.config.lab_bdi_upd_uboot_bdi_prompt)
-searchlist = ["Connection refused"]
+searchlist = ["Connection refused", "No route"]
 tmp = True
 while tmp == True:
     ret = tb.tbot_read_line_and_check_strings(c, searchlist)
-    if ret == '0':
+    if ret == '0' or ret == '1':
         # back to linux
         c.set_prompt(tb.config.linux_prompt)
         tb.tbot_expect_prompt(c)
