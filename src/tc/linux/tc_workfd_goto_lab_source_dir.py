22e47100b5a97c93ce7d10bbbd0e96ae9a0256d0
Heiko Schocher <hs@denx.de>
2017-01-06 06:19:01 +0100

    tc: tc_workfd_goto_lab_source_dir.py: introduce TBOT_WORKDIR
    set bash variable tc_workfd_goto_lab_source_dir.py to tbot
    work directory, and use it.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_goto_lab_source_dir.py
+++ src/tc/linux/tc_workfd_goto_lab_source_dir.py
@@ -15,13 +15,31 @@
 # start with
 # python2.7 src/common/tbot.py -c tbot.cfg -t tc_workfd_goto_lab_source_dir.py
 # switch into lab PC source directory tb.config.tc_lab_source_dir
+# set TBOT_WORKDIR to tb.config.tc_lab_source_dir
 # End:
 
 from tbotlib import tbot
 
 logging.info("args: %s", tb.workfd)
 
-tmp = "cd " + tb.config.tc_lab_source_dir
+try:
+    tb.workfd.tc_lab_source_dir_checked
+except:
+    tb.workfd.tc_lab_source_dir_checked = False
+
+if tb.workfd.tc_lab_source_dir_checked == False:
+    tmp = 'export TBOT_WORKDIR=' + tb.config.tc_lab_source_dir
+    tb.write_lx_cmd_check(tb.workfd, tmp)
+
+    tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_WORKDIR'
+    ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
+    if ret == 'False':
+        tmp = 'mkdir $TBOT_WORKDIR'
+        tb.write_lx_cmd_check(tb.workfd, tmp)
+
+    tb.workfd.tc_lab_source_dir_checked = True
+
+tmp = "cd $TBOT_WORKDIR"
 tb.write_lx_cmd_check(tb.workfd, tmp)
 
 tb.end_tc(True)
