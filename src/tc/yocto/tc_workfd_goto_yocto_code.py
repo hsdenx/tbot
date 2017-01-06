0d240378d8925617856442b0a88983aaf6fb0b8e
Heiko Schocher <hs@denx.de>
2017-01-06 06:18:06 +0100

    tc: yocto: introduce TBOT_YOCTO_WORKDIR
    create bash variable TBOT_YOCTO_WORKDIR and use it
    as path.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/yocto/tc_workfd_goto_yocto_code.py
+++ src/tc/yocto/tc_workfd_goto_yocto_code.py
@@ -17,17 +17,34 @@
 # switch into yocto source tb.config.tc_lab_source_dir + "/yocto-" + tb.config.boardlabname
 # set tb.config.yocto_name to "yocto-" + tb.config.boardlabname
 # and tb.config.yocto_fulldir_name to tb.config.tc_lab_source_dir + "/" + tb.config.yocto_name
+# and set $TBOT_YOCTO_WORKDIR to tb.config.yocto_fulldir_name
 # End:
 
 from tbotlib import tbot
 
 logging.info("args: %s", tb.workfd)
 
-# set some global config variables
-tb.config.yocto_name = "yocto-" + tb.config.boardlabname
-tb.config.yocto_fulldir_name = tb.config.tc_lab_source_dir + "/" + tb.config.yocto_name
+try:
+    tb.workfd.tc_workfd_goto_yocto_code_checked
+except:
+    tb.workfd.tc_workfd_goto_yocto_code_checked = False
+
+if tb.workfd.tc_workfd_goto_yocto_code_checked == False:
+    # set some global config variables
+    tb.config.yocto_name = "yocto-" + tb.config.boardlabname
+    tb.config.yocto_fulldir_name = tb.config.tc_lab_source_dir + "/" + tb.config.yocto_name
+
+    tmp = 'export TBOT_YOCTO_WORKDIR=' + tb.config.yocto_fulldir_name
+    tb.write_lx_cmd_check(tb.workfd, tmp)
+
+    tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_YOCTO_WORKDIR'
+    ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
+    if ret == 'False':
+        tb.workfd.tc_workfd_goto_yocto_code_checked = True
+        tb.end_tc(False)
+ 
 # cd into yocto code
-tmp = "cd " + tb.config.yocto_fulldir_name
+tmp = "cd $TBOT_YOCTO_WORKDIR"
 tb.write_lx_cmd_check(tb.workfd, tmp)
 
 tb.end_tc(True)
