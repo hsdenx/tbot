7ea903ba6ca7ecbdabb114ca42b3c2fbb355d427
Heiko Schocher <hs@denx.de>
2017-01-09 06:39:04 +0100

    tc_workfd_goto_linux_code.py: rework testcase
    switch into linux source tb.config.tc_lab_source_dir + "/linux-" + tb.config.boardlabname
    set tb.config.linux_name to "linux-" + tb.config.boardlabname
    and tb.config.linux_fulldir_name to tb.config.tc_lab_source_dir + "/" + tb.config.linux_name
    and set $TBOT_BASEDIR_LINUX to tb.config.linux_fulldir_name
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_goto_linux_code.py
+++ src/tc/linux/tc_workfd_goto_linux_code.py
@@ -15,13 +15,35 @@
 # start with
 # python2.7 src/common/tbot.py -c tbot.cfg -t tc_workfd_goto_linux_code.py
 # switch into linux source tb.config.tc_lab_source_dir + "/linux-" + tb.config.boardlabname
+# set tb.config.linux_name to "linux-" + tb.config.boardlabname
+# and tb.config.linux_fulldir_name to tb.config.tc_lab_source_dir + "/" + tb.config.linux_name
+# and set $TBOT_BASEDIR_LINUX to tb.config.linux_fulldir_name
 # End:
 
 from tbotlib import tbot
 
 logging.info("args: %s", tb.workfd)
 
-tmp = "cd " + tb.config.tc_lab_source_dir + "/linux-" + tb.config.boardlabname
+try:
+    tb.workfd.tc_workfd_goto_linux_code_checked
+except:
+    tb.workfd.tc_workfd_goto_linux_code_checked = False
+
+if tb.workfd.tc_workfd_goto_linux_code_checked == False:
+    # set some global config variables
+    tb.config.linux_name = "linux-" + tb.config.boardlabname
+    tb.config.linux_fulldir_name = "$TBOT_BASEDIR/" + tb.config.linux_name
+
+    tmp = 'export TBOT_BASEDIR_LINUX=' + tb.config.linux_fulldir_name
+    tb.write_lx_cmd_check(tb.workfd, tmp)
+
+    tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_BASEDIR_LINUX'
+    ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
+    if ret == 'False':
+        tb.workfd.tc_workfd_goto_linux_code_checked = True
+        tb.end_tc(False)
+
+tmp = "cd $TBOT_BASEDIR_LINUX"
 tb.write_lx_cmd_check(tb.workfd, tmp)
 
 tb.end_tc(True)
