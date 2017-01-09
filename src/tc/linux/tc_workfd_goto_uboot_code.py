7e43bbdf9266098567f6d790833e31ef086f5fa1
Heiko Schocher <hs@denx.de>
2017-01-09 06:34:19 +0100

    TBOT_UBOOT_WORKDIR -> TBOT_BASEDIR_UBOOT
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_goto_uboot_code.py
+++ src/tc/linux/tc_workfd_goto_uboot_code.py
@@ -17,7 +17,7 @@
 # switch into U-Boot source tb.config.tc_lab_source_dir + "/u-boot-" + tb.config.boardlabname
 # set tb.config.uboot_name to "u-boot-" + tb.config.boardlabname
 # and tb.config.uboot_fulldir_name to tb.config.tc_lab_source_dir + "/" + tb.config.uboot_name
-# and set $TBOT_UBOOT_WORKDIR to tb.config.uboot_fulldir_name
+# and set $TBOT_BASEDIR_UBOOT to tb.config.uboot_fulldir_name
 #
 # End:
 
@@ -35,16 +35,16 @@ if tb.workfd.tc_workfd_goto_uboot_code_checked == False:
     tb.config.uboot_name = "u-boot-" + tb.config.boardlabname
     tb.config.uboot_fulldir_name = "$TBOT_BASEDIR/" + tb.config.uboot_name
 
-    tmp = 'export TBOT_UBOOT_WORKDIR=' + tb.config.uboot_fulldir_name
+    tmp = 'export TBOT_BASEDIR_UBOOT=' + tb.config.uboot_fulldir_name
     tb.write_lx_cmd_check(tb.workfd, tmp)
 
-    tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_UBOOT_WORKDIR'
+    tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_BASEDIR_UBOOT'
     ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
     if ret == 'False':
         tb.workfd.tc_workfd_goto_uboot_code_checked = True
         tb.end_tc(False)
 
-tmp = "cd $TBOT_UBOOT_WORKDIR"
+tmp = "cd $TBOT_BASEDIR_UBOOT"
 tb.write_lx_cmd_check(tb.workfd, tmp)
 
 tb.end_tc(True)
