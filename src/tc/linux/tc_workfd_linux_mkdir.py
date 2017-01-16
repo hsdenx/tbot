cf347eaa5fb03038bd246a6c6f40a3083230e19f
Heiko Schocher <hs@denx.de>
2017-01-16 14:43:55 +0100

    new tc: tc_workfd_linux_mkdir.py
    check if the directory tb.config.tc_workfd_linux_mkdir_dir exists.
    if not, create it
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/linux/tc_workfd_linux_mkdir.py
@@ -0,0 +1,31 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Description:
+# start with
+# python2.7 src/common/tbot.py -c tbot.cfg -t tc_workfd_linux_mkdir.py
+# check if the directory tb.config.tc_workfd_linux_mkdir_dir exists.
+# if not, create it
+# End:
+
+from tbotlib import tbot
+
+logging.info("args: workfd: %s %s", tb.workfd, tb.config.tc_workfd_linux_mkdir_dir)
+
+tb.config.tc_workfd_check_if_dir_exists_name = tb.config.tc_workfd_linux_mkdir_dir
+ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
+if ret == False:
+    cmd = 'mkdir -p ' + tb.config.tc_workfd_linux_mkdir_dir
+    tb.write_lx_cmd_check(tb.workfd, cmd)
+
+tb.end_tc(True)
