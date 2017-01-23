60e6d0ac7dfc948a2ed79352cf7548e60ca1ff51
Heiko Schocher <hs@denx.de>
2017-01-23 08:40:03 +0100

    board: corvus: use tc_workfd_set_toolchain.py
    use tc tc_workfd_set_toolchain.py for setting
    the toolchain. With newest U-Boot setting toolchain
    with the eldk-switch script prints error:
    
    HOSTLD tools/mkimage
    tb_ctrl: Traceback (most recent call last):
    File "./lib/libfdt/setup.py", line 7, in <module>
    from distutils.core import setup, Extension
    ImportError: No module named distutils.core
    tools/Makefile:120: recipe for target 'tools/_libfdt.so' failed
    make[1]: *** [tools/_libfdt.so] Error 1
    Makefile:1217: recipe for target 'tools' failed
    make: *** [tools] Error 2
    hs@pollux [ 7:24:18] ttbott>
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- all_denx_boards.sh
+++ all_denx_boards.sh
@@ -10,4 +10,4 @@ tbot.py -s lab_denx -c fipad -t tc_board_fipad_linux.py -v log/fipad-$logfile_en
 tbot.py -s lab_denx -c shc -t tc_board_shc_ub_tests.py -v log/shc-$logfile_end
 tbot.py -s lab_denx -c smartweb -t tc_board_smartweb.py -v log/smartweb-$logfile_end
 tbot.py -s lab_denx -c corvus -t tc_board_corvus.py -v log/corvus-$logfile_end
-tbot.py -s lab_denx -c corvus -t tc_board_tqm5200s_try_cur_ub.py -v log/tqm5200-$logfile_end
+tbot.py -s lab_denx -c tqm5200 -t tc_board_tqm5200s_try_cur_ub.py -v log/tqm5200-$logfile_end
