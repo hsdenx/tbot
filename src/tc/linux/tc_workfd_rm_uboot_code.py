76f575598be165f8028cc4310d8b1703f9836279
Constantin Schuett <fixed-term.constantin.schuett@de.bosch.com>
2017-05-23 09:24:28 +0200

    Use tc_workfd_rm_dir to remove u-boot code
    Signed-off-by: Constantin Schuett <fixed-term.constantin.schuett@de.bosch.com>
    Signed-off-by: Mark Jonas <mark.jonas@de.bosch.com>

--- src/tc/linux/tc_workfd_rm_uboot_code.py
+++ src/tc/linux/tc_workfd_rm_uboot_code.py
@@ -22,6 +22,6 @@ from tbotlib import tbot
 logging.info("args: %s", tb.workfd)
 
 tb.config.tc_lab_rm_dir = tb.config.tc_lab_source_dir + '/u-boot-' + tb.config.boardlabname
-tb.eof_call_tc("tc_lab_rm_dir.py")
+tb.eof_call_tc("tc_workfd_rm_dir.py")
 
 tb.end_tc(True)
