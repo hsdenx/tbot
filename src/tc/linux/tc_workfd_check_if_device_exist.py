94a9b1eabb69b7e16423ab16de2a808fb321305a
Heiko Schocher <hs@denx.de>
2017-05-30 14:53:45 +0200

    new testcase: tc_workfd_check_if_device_exist.py
    check if a device tb.config.tc_workfd_check_if_device_exists_name exist
    this tc returns always true, but sets
    tb.config.tc_return True or False, because we may not
    want to end testcase failed, if device not exists.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/linux/tc_workfd_check_if_device_exist.py
@@ -0,0 +1,29 @@
+# This file is part of tbot.  tbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Description:
+# start with
+# python2.7 src/common/tbot.py -c tbot.cfg -t tc_workfd_check_if_device_exist.py
+# check if a device tb.config.tc_workfd_check_if_device_exists_name exist
+# this tc returns always true, but sets
+# tb.config.tc_return True or False, because we may not
+# want to end testcase failed, if device not exists.
+# End:
+
+from tbotlib import tbot
+
+logging.info("args: workfd %s %s", tb.workfd, tb.config.tc_workfd_check_if_device_exists_name)
+
+tmp = 'test -c ' + tb.config.tc_workfd_check_if_device_exists_name
+tb.write_lx_cmd_check(tb.workfd, tmp)
+tb.end_tc(True)
