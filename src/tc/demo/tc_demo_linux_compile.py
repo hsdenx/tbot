4accc107da131f7ac356612ca3a163763ec59fd8
Heiko Schocher <hs@denx.de>
2017-08-28 12:03:54 +0200

    tc_demo_linux_test.py: seperate linux compile and deploy
    seperate the tasks compile and deploy linux in a new
    testcases named:
    
    src/tc/demo/tc_demo_linux_compile.py
    
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Signed-off-by: Mark Jonas <mark.jonas@de.bosch.com>

--- src/tc/demo/tc_demo_linux_compile.py
+++ src/tc/demo/tc_demo_linux_compile.py
@@ -12,38 +12,52 @@
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Description:
-# start with
-# tbot.py -s lab_denx -c beagleboneblack -t tc_demo_linux_test.py
-# get linux code and compile it for a board, then call testcase
 #
-# src/tc/demo/tc_demo_linux_testcases.py
-#
-# if tb.config.tc_demo_linux_test_deploy != ''
-# call this testcase for deploying the resulting images.
+# - goto linux code
+# - compile it
+# - deploy it
 #
 # End:
 
 from tbotlib import tbot
 
 try:
+    tb.c_cpc
+    tb.workfd = tb.c_cpc
+except:
+    tb.workfd = tb.c_ctrl
+
+try:
     tb.config.tc_demo_linux_test_deploy
 except:
     tb.config.tc_demo_linux_test_deploy = ''
 
-tb.workfd = tb.c_ctrl
-
 logging.info("args: %s %s", tb.workfd.name, tb.config.tc_demo_linux_test_deploy)
-
-# delete old linux source tree
-tb.eof_call_tc("tc_workfd_rm_linux_code.py")
-
-tb.eof_call_tc("tc_workfd_get_linux_source.py")
 
 tb.eof_call_tc("tc_workfd_goto_linux_code.py")
 
 # compile it
-tb.eof_call_tc("tc_demo_linux_compile.py")
+tb.eof_call_tc("tc_workfd_compile_linux.py")
 
-tb.eof_call_tc("tc_demo_linux_testcases.py")
+if tb.config.tc_demo_linux_test_deploy != '':
+    tb.eof_call_tc(tb.config.tc_demo_linux_test_deploy)
+    tb.end_tc(True)
+
+# copy files to tftpdir
+c = tb.workfd
+tb.statusprint("copy files")
+r = tb.config.tftprootdir
+t = r + tb.config.tftpboardname + '/' + tb.config.ub_load_board_env_subdir
+s = "$TBOT_BASEDIR_LINUX/arch/arm/boot/"
+so = s + tb.config.tc_workfd_compile_linux_make_target
+ta = t
+tb.eof_call_tc("tc_lab_cp_file.py", ch=c, s=so, t=ta)
+for f in tb.config.tc_workfd_compile_linux_dt_name:
+    so = s + 'dts/' + f
+    tb.eof_call_tc("tc_lab_cp_file.py", ch=c, s=so, t=ta)
+
+so = "$TBOT_BASEDIR_LINUX/System.map"
+ta = t + "linux-system.map"
+tb.eof_call_tc("tc_lab_cp_file.py", ch=c, s=so, t=ta)
 
 tb.end_tc(True)
