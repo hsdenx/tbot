e57dd7133e96a95aba19a2d2f2903089a3a9acf0
Heiko Schocher <hs@denx.de>
2017-08-29 15:35:20 +0200

    doc, u-boot: allow longer lines in pdf
    set line length to 125
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/documentation/make_doku.sh
+++ src/documentation/make_doku.sh
@@ -17,7 +17,7 @@ do
   fi
 done
 
-python2.7 replace_tbot_marker.py -i index.rst -o work/index.rst -t logfiles/ -r True -l rst -w 100
+python2.7 replace_tbot_marker.py -i index.rst -o work/index.rst -t logfiles/ -r True -l rst -w 125
 rst2pdf -s stylesheet.txt work/index.rst pdf/index.pdf
 python2.7 mark_red.py -i pdf/index.pdf -o pdf/dulg_bbb.pdf;okular pdf/dulg_bbb.pdf
 
