4a0ad17147581b99aecabf788bd2b495a775e150
Heiko Schocher <hs@denx.de>
2017-09-08 05:24:51 +0000

    tc_workfd_iperf.py: support iperf3
    if you have not the iperf cmd instead iperf 3, you can
    set
    tb.config.tc_workfd_c_sr_vers or tb.config.tc_workfd_c_sl_vers
    to '3'
    
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Signed-off-by: Mark Jonas <mark.jonas@de.bosch.com>

--- src/tc/linux/tc_workfd_iperf.py
+++ src/tc/linux/tc_workfd_iperf.py
@@ -14,6 +14,7 @@
 # Description:
 # start with
 # python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_iperf.py
+#
 # make a minimal iperf check
 # starts an iperf server on tb.tc_workfd_c_sr connection
 #   with ip addr tb.tc_workfd_iperf_sip
@@ -21,26 +22,46 @@
 # waiting for the first result of iperf measure and
 # check if the resulting speed is bigger then
 # tb.tc_workfd_iperf_minval
+#
+# if you have not the iperf cmd instead iperf 3, you can
+# set
+# tb.config.tc_workfd_c_sr_vers or tb.config.tc_workfd_c_sl_vers
+# to '3'
 # End:
 
 from tbotlib import tbot
 
+try:
+    tb.config.tc_workfd_c_sr_vers
+except:
+    tb.config.tc_workfd_c_sr_vers = ''
+
+try:
+    tb.config.tc_workfd_c_sl_vers
+except:
+    tb.config.tc_workfd_c_sl_vers = ''
+
 logging.info("args: workfd %s %s %s %s", tb.tc_workfd_c_sr.name, tb.tc_workfd_c_sl.name, tb.tc_workfd_iperf_sip, tb.tc_workfd_iperf_minval)
+logging.info("args: %s %s", tb.config.tc_workfd_c_sr_vers, tb.config.tc_workfd_c_sl_vers)
 
 tb.set_board_state("linux")
 
 c_s = tb.tc_workfd_c_sr
 c_t = tb.tc_workfd_c_sl
 
-cmd = 'iperf -s'
-cmd_sl = 'iperf -c ' + tb.tc_workfd_iperf_sip
+cmd_sr = 'iperf' + tb.config.tc_workfd_c_sr_vers + ' -s'
+cmd_sl = 'iperf' + tb.config.tc_workfd_c_sl_vers + ' -c ' + tb.tc_workfd_iperf_sip
 
-tb.eof_write(c_s, cmd)
+tb.eof_write(c_s, cmd_sr)
 searchlist = ["Server listening", "TCP window size"]
 s_st = False
 tmp = True
 while tmp == True:
     ret = tb.tbot_rup_and_check_strings(c_s, searchlist)
+    if tb.config.tc_workfd_c_sr_vers == '3':
+        if ret == '0':
+            tmp = False
+            s_st = True
     if ret == '1':
         tmp = False
         s_st = True
@@ -51,7 +72,11 @@ if s_st != True:
     tb.end_tc(False)
 
 tb.eof_write(c_t, cmd_sl)
-searchlist = ["connected with", "Bandwidth", "\n"]
+if tb.config.tc_workfd_c_sl_vers == '3':
+    searchlist = ["connected", "Bandwidth", "\n"]
+else:
+    searchlist = ["connected with", "Bandwidth", "\n"]
+
 got_line = False
 result = False
 tmp = True
@@ -69,7 +94,12 @@ while tmp == True:
         tmp2 = tb.buf.split()
         if len(tmp2) < 8:
             continue
-        if tmp2[2] == '0.0-10.0':
+        if tb.config.tc_workfd_c_sl_vers == '3':
+            timestring = '0.00-10.00'
+        else:
+            timestring = '0.0-10.0'
+
+        if tmp2[2] == timestring:
             val = float(tmp2[6])
             if val > tb.tc_workfd_iperf_minval:
                 result = True
