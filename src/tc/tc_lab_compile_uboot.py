1e6b4563931fd56366537ed37ab825ccfdbb1601
Heiko Schocher <hs@denx.de>
2017-09-12 06:05:41 +0000

    tc_lab_compile_uboot.py: use tc_workfd_compile_uboot.py
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Signed-off-by: Mark Jonas <mark.jonas@de.bosch.com>

--- src/tc/tc_lab_compile_uboot.py
+++ src/tc/tc_lab_compile_uboot.py
@@ -14,34 +14,19 @@
 # Description:
 # start with
 # python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_lab_compile_uboot.py
-# compile u-boot
+#
+# set workfd to c_ctrl
+# call tc_workfd_compile_uboot.py
+# restore old workfd
+#
 # End:
 
 from tbotlib import tbot
 
-tb.eof_call_tc("tc_workfd_goto_uboot_code.py")
-
 savefd = tb.workfd
 tb.workfd = tb.c_ctrl
-if tb.config.tc_lab_compile_uboot_export_path != 'none':
-    tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_set_dtc')
-    tmp = "export PATH=" + tb.config.tc_lab_compile_uboot_export_path + ":$PATH"
-    tb.write_lx_cmd_check(tb.workfd, tmp)
 
-tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_mrproper')
-tmp = "make mrproper"
-tb.write_lx_cmd_check(tb.workfd, tmp)
-
-tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_config')
-defname = tb.config.tc_lab_compile_uboot_boardname + "_defconfig"
-tmp = "make " + defname
-tb.event.create_event('main', tb.config.boardname, "UBOOT_DEFCONFIG", defname)
-tb.write_lx_cmd_check(tb.workfd, tmp)
-tb.event.create_event('main', tb.config.boardname, "UBOOT_SRC_PATH", tb.config.tc_lab_source_dir + "/u-boot-" + tb.config.boardlabname)
-
-tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_make')
-tmp = "make " + tb.config.tc_lab_compile_uboot_makeoptions + " all"
-tb.write_lx_cmd_check(tb.workfd, tmp)
+tb.eof_call_tc("tc_workfd_compile_uboot.py")
 
 tb.workfd = savefd
 tb.end_tc(True)
--- src/tc/tc_workfd_compile_uboot.py
+++ src/tc/tc_workfd_compile_uboot.py
@@ -19,22 +19,28 @@
 
 from tbotlib import tbot
 
+tb.eof_call_tc("tc_workfd_goto_uboot_code.py")
+
 if tb.config.tc_lab_compile_uboot_export_path != 'none':
+    tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_set_dtc')
     tmp = "printenv PATH | grep --color=never " + tb.config.tc_lab_compile_uboot_export_path
     ret = tb.write_lx_cmd_check(tb.workfd, tmp, endTC=False)
     if ret == False:
         tmp = "export PATH=" + tb.config.tc_lab_compile_uboot_export_path + ":$PATH"
         tb.write_lx_cmd_check(tb.workfd, tmp)
 
+tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_mrproper')
 tmp = "make mrproper"
 tb.write_lx_cmd_check(tb.workfd, tmp)
 
+tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_config')
 defname = tb.config.tc_lab_compile_uboot_boardname + "_defconfig"
 tmp = "make " + defname
 tb.event.create_event('main', tb.config.boardname, "UBOOT_DEFCONFIG", defname)
 tb.write_lx_cmd_check(tb.workfd, tmp)
 tb.event.create_event('main', tb.config.boardname, "UBOOT_SRC_PATH", tb.config.tc_lab_source_dir + "/u-boot-" + tb.config.boardlabname)
 
+tb.event.create_event('main', 'tc_lab_compile_uboot.py', 'SET_DOC_FILENAME', 'compile_uboot_make')
 tmp = "make " + self.config.tc_lab_compile_uboot_makeoptions + " all"
 tb.write_lx_cmd_check(tb.workfd, tmp)
 
