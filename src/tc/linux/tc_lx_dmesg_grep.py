f094d3dcd194433b0eb4361e4a0619a9fa81a600
Heiko Schocher <hs@denx.de>
2017-09-26 09:02:41 +0000

    tc_lx_dmesg_grep.py: make grep options configurable
    make the grep options configurable through tb.config.tc_lx_dmesg_grep_options
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_lx_dmesg_grep.py
+++ src/tc/linux/tc_lx_dmesg_grep.py
@@ -12,22 +12,28 @@
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_lx_dmesg_grep.py
+#
 # check if string tb.config.tc_lx_dmesg_grep_name is in dmesg output.
+# make the grep options configurable through tb.config.tc_lx_dmesg_grep_options
+#
 # End:
 
 from tbotlib import tbot
 
+try:
+    tb.config.tc_lx_dmesg_grep_options
+except:
+    tb.config.tc_lx_dmesg_grep_options = '--color=never'
+
 # here starts the real test
-logging.info("args: %s", tb.config.tc_lx_dmesg_grep_name)
+logging.info("args: %s %s", tb.config.tc_lx_dmesg_grep_name, tb.config.tc_lx_dmesg_grep_options)
 
 tb.workfd = tb.c_con
 c = tb.workfd
 # set board state for which the tc is valid
 tb.set_board_state("linux")
 
-tmp = 'dmesg | grep --color=never \'' + tb.config.tc_lx_dmesg_grep_name + '\''
+tmp = 'dmesg | grep ' + tb.config.tc_lx_dmesg_grep_options + ' \'' + tb.config.tc_lx_dmesg_grep_name + '\''
 tb.eof_write(c, tmp)
 tb.tbot_expect_prompt(c)
 #tb.eof_expect_string(c, tb.config.tc_lx_dmesg_grep_name)
