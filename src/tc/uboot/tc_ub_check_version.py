2ed7fb35a4e5a429c3527dbf44c264514c2bf994
Heiko Schocher <hs@denx.de>
2017-09-26 06:53:43 +0000

    tc_ub_check_version.py: do not test u-boot vers if tb.uboot_vers == ''
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_ub_check_version.py
+++ src/tc/uboot/tc_ub_check_version.py
@@ -12,10 +12,10 @@
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_ub_check_version.py
+#
 # check if the current running U-Boot vers == tb.uboot_vers
 # and SPL vers == tb.spl_vers
+#
 # End:
 
 from tbotlib import tbot
@@ -28,29 +28,30 @@ tb.set_board_state("u-boot")
 # set env var
 c = tb.c_con
 
-tmp = 'vers'
-tb.eof_write(c, tmp)
-searchlist = ['U-Boot 20']
-tmp = True
-ret = False
-uvers = 'undef'
-while tmp == True:
-    retu = tb.tbot_rup_and_check_strings(c, searchlist)
-    if retu == 'prompt':
-        tmp = False
-    if retu == '0':
-        ret = tb.tbot_rup_and_check_strings(c, '\n')
-        if ret == 'prompt':
-            tb.enc_tc(False)
-        tmp = 'U-Boot 20' + tb.buf.replace('\r','')
-        uvers = tmp.replace('\n','')
-        if uvers == tb.uboot_vers:
-            ret = True
-        tmp = True
+if tb.uboot_vers != '':
+    tmp = 'vers'
+    tb.eof_write(c, tmp)
+    searchlist = ['U-Boot 20']
+    tmp = True
+    ret = False
+    uvers = 'undef'
+    while tmp == True:
+        retu = tb.tbot_rup_and_check_strings(c, searchlist)
+        if retu == 'prompt':
+            tmp = False
+        if retu == '0':
+            ret = tb.tbot_rup_and_check_strings(c, '\n')
+            if ret == 'prompt':
+                tb.enc_tc(False)
+            tmp = 'U-Boot 20' + tb.buf.replace('\r','')
+            uvers = tmp.replace('\n','')
+            if uvers == tb.uboot_vers:
+                ret = True
+            tmp = True
 
-if ret != True:
-    logging.warn("UB Vers differ %s != %s", uvers, tb.uboot_vers)
-    tb.end_tc(False)
+    if ret != True:
+        logging.warn("UB Vers differ %s != %s", uvers, tb.uboot_vers)
+        tb.end_tc(False)
 
 if tb.spl_vers == '':
     tb.end_tc(True)
@@ -73,9 +74,12 @@ while tmp == True:
         splvers = tmp.replace('\n','')
         if splvers == tb.spl_vers:
             ret = True
-        tmp = True
+        tmp = False
+
+tb.set_board_state("u-boot")
 
 if ret == False:
     logging.warn("UB SPL Vers differ %s != %s", splvers, tb.spl_vers)
+
 
 tb.end_tc(ret)
