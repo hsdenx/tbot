7e75e8e38eb1c169cd5bc2ed33042acfbb882d22
Heiko Schocher <hs@denx.de>
2017-10-17 10:15:36 +0200

    config/lab_denx.py: fixes after tbot2go changes
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- config/lab_denx.py
+++ config/lab_denx.py
@@ -8,13 +8,14 @@ channel_timeout = 0.5
 loglevel = 'INFO'
 wdt_timeout = '900'
 
-tc_workfd_work_dir = '/work/hs/tbot'
+tc_workfd_work_dir = '/work/hs/tbot/'
 lab_tmp_dir = '/var/tmp/'
-tftpdir = '/tftpboot'
+tftpdir = '/tftpboot/'
 
 # lab specific changes for the mbconnect lab
 def set_labspecific(tb):
     if tb.config.boardname == 'am335x_evm':
+        tb.config.tc_ub_boot_linux_load_env = 'set'
 	tb.config.ub_load_board_env_set = [
 		'setenv serverip 192.168.1.1',
 		'setenv netmask 255.255.0.0',
@@ -31,3 +32,4 @@ def set_labspecific(tb):
         tb.config.tc_lab_denx_connect_to_board_tc = 'tc_workfd_connect_with_kermit.py'
         tb.config.tc_workfd_connect_with_kermit_rlogin = 'rlogin metis bbb'
         tb.config.tc_lab_denx_disconnect_from_board_tc = 'tc_workfd_disconnect_with_kermit.py'
+        tb.config.tc_board_bootmode_tc = 'tc_board_bbb_bootmode_labdenx.py'
