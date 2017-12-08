dbd41c0da385c18005fec03d03b636602113efb5
Heiko Schocher <hs@denx.de>
2017-12-08 09:56:06 +0000

    tc_connect_to_compilepc.py: connect over more than 1 ssh
    make it possible to connect over more than one ssh
    connections in a row to the compile PC.
    
    Therefore, the vars
    tb.config.compile_pc_ip,
    tb.config.compile_pc_user
    tb.config.connect_to_compilepc_ssh_opt
    tb.config.connect_to_compilepc_ssh_cmd_prompt
    
    could be a list of strings.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_connect_to_compilepc.py
+++ src/tc/linux/tc_connect_to_compilepc.py
@@ -20,6 +20,10 @@
 #
 # ! workfd is set after calling this testcase to the new connection !
 #
+# The vars tb.config.compile_pc_ip, tb.config.compile_pc_user
+# tb.config.connect_to_compilepc_ssh_opt, tb.config.connect_to_compilepc_ssh_cmd_prompt
+# could also be a list of strings.
+#
 # End:
 
 from tbotlib import tbot
@@ -39,18 +43,47 @@ except:
     tb.check_open_fd(tb.c_cpc)
 
 tb.workfd = tb.c_cpc
-cur_p = tb.workfd.get_prompt_type
 
-if cur_p != tb.config.compile_pc_prompt:
-    # we are not @ compile PC
-    # go to compile PC
-    tb.config.workfd_ssh_cmd = tb.config.compile_pc_user + '@' + tb.config.compile_pc_ip
-    tb.config.tc_workfd_ssh_opt = tb.config.connect_to_compilepc_ssh_opt
-    tb.config.workfd_ssh_cmd_prompt = tb.config.connect_to_compilepc_ssh_cmd_prompt
-    tb.eof_call_tc("tc_workfd_ssh.py")
-    # we should set in tc_workfd_ssh.py the new prompt !
-    # and check in tc_workfd_ssh.py if we have the correct prompt!
-    # TODO
-    tb.set_prompt(tb.c_cpc, tb.config.compile_pc_prompt, 'linux')
+if type(tb.config.compile_pc_ip) != list:
+    ips = []
+    ips.append(tb.config.compile_pc_ip)
+else:
+    ips = tb.config.compile_pc_ip
+
+if type(tb.config.compile_pc_user) != list:
+    users = []
+    users.append(tb.config.compile_pc_user)
+else:
+    users = tb.config.compile_pc_user
+
+if type(tb.config.connect_to_compilepc_ssh_opt) != list:
+    opts = []
+    opts.append(tb.config.connect_to_compilepc_ssh_opt)
+else:
+    opts = tb.config.connect_to_compilepc_ssh_opt
+
+if type(tb.config.connect_to_compilepc_ssh_cmd_prompt) != list:
+    prompts = []
+    prompts.append(tb.config.connect_to_compilepc_ssh_cmd_prompt)
+else:
+    prompts = tb.config.connect_to_compilepc_ssh_cmd_prompt
+
+cons = zip(ips, users, opts, prompts)
+
+print ("CONS ", cons)
+for con in cons:
+    cur_p = tb.workfd.get_prompt_type
+
+    if cur_p != tb.config.compile_pc_prompt:
+        # we are not @ compile PC
+        # go to compile PC
+        tb.config.workfd_ssh_cmd = con[1] + '@' + con[0]
+        tb.config.tc_workfd_ssh_opt = con[2]
+        tb.config.workfd_ssh_cmd_prompt = con[3]
+        tb.eof_call_tc("tc_workfd_ssh.py")
+        # we should set in tc_workfd_ssh.py the new prompt !
+        # and check in tc_workfd_ssh.py if we have the correct prompt!
+        # TODO
+        tb.set_prompt(tb.c_cpc, tb.config.compile_pc_prompt, 'linux')
 
 tb.end_tc(True)
