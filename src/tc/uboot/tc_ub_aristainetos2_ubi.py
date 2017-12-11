49b8e7e8b323773002f6cd177d151543b15e741d
Heiko Schocher <hs@denx.de>
2017-12-11 12:58:21 +0100

    tc_ub_aristainetos2_ubi.py: test ubi written iamge after reboot
    also test if a ubi image is read correct after a reset of U-Boot.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_ub_aristainetos2_ubi.py
+++ src/tc/uboot/tc_ub_aristainetos2_ubi.py
@@ -30,7 +30,7 @@ time.sleep(10)
 tb.eof_call_tc("tc_ub_load_board_env.py")
 
 # probe spi nor
-tb.eof_write_con("sf probe")
+tb.eof_write_cmd(tb.c_con, "sf probe")
 
 # nand tests
 tb.config.tc_ub_ubi_prep_offset = '4096'
@@ -76,6 +76,36 @@ tb.eof_call_tc("tc_ub_ubifs_mount.py")
 # ls "/"
 tb.config.tc_ub_ubifs_ls_dir = '/'
 tb.eof_call_tc("tc_ub_ubifs_ls.py")
+
+# now reset the board
+tb.eof_call_tc("tc_ub_reset.py")
+# ... and reread the image again
+
+tb.eof_write_cmd(tb.c_con, "sf probe")
+tb.config.tc_ub_ubi_prep_offset = '4096'
+tb.config.tc_ub_ubi_prep_partname = 'ubi'
+tb.eof_call_tc("tc_ub_ubi_prepare.py")
+
+# get file
+tb.config.tc_ub_tftp_file_addr = tb.config.tc_ub_ubi_write_addr
+tb.config.tc_ub_tftp_file_name = '/tftpboot/aristainetos/tbot/rootfs-minimal.ubifs'
+tb.eof_call_tc("tc_ub_tftp_file.py")
+
+tb.eof_call_tc("tc_ub_get_filesize.py")
+tb.config.tc_ub_ubi_write_len = tb.ub_filesize
+
+# read file from ubi volume
+tb.tc_ub_ubi_read_addr = '11000000'
+tb.tc_ub_ubi_read_vol_name = tb.config.tc_ub_ubi_write_vol_name
+tb.tc_ub_ubi_read_len = tb.config.tc_ub_ubi_write_len
+tb.eof_call_tc("tc_ub_ubi_read.py")
+
+# cmp if all bytes are the same
+tb.tc_ub_cmp_addr1 = tb.tc_ub_ubi_read_addr
+tb.tc_ub_cmp_addr2 = tb.config.tc_ub_tftp_file_addr
+tb.tc_ub_cmp_len = tb.config.tc_ub_ubi_write_len
+tb.eof_call_tc("tc_ub_cmp.py")
+
 
 # nor tests
 tb.config.tc_ub_ubi_prep_offset = '64'
