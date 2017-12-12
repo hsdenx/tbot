981f2fad866601cd230a2631b0a09b1a6edbb79d
Heiko Schocher <hs@denx.de>
2017-12-12 12:29:50 +0000

    tc_workfd_check_if_dir_exist.py: if dir not exist, create it
    if tb.config.tc_workfd_check_if_dir_exists_create != 'no'
    create the directory, if it does not exist.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_check_if_dir_exist.py
+++ src/tc/linux/tc_workfd_check_if_dir_exist.py
@@ -18,12 +18,29 @@
 # this tc returns always true, but sets
 # tb.config.tc_return True or False, because we may not
 # want to end testcase failed, if dir not exists.
+#
+# if tb.config.tc_workfd_check_if_dir_exists_create != 'no'
+# create the directory.
+#
 # End:
 
 from tbotlib import tbot
 
+try:
+    tb.config.tc_workfd_check_if_dir_exists_create
+except:
+    tb.config.tc_workfd_check_if_dir_exists_create = 'no'
+
 logging.info("args: workfd %s %s", tb.workfd, tb.config.tc_workfd_check_if_dir_exists_name)
 
 tmp = 'test -d ' + tb.config.tc_workfd_check_if_dir_exists_name
-tb.write_lx_cmd_check(tb.workfd, tmp)
+ret = tb.write_lx_cmd_check(tb.workfd, tmp, endTC=False)
+if ret == True:
+    tb.end_tc(True)
+
+if tb.config.tc_workfd_check_if_dir_exists_create == 'no':
+    tb.end_tc(False)
+
+cmd = 'mkdir -p ' + tb.config.tc_workfd_check_if_dir_exists_name
+tb.write_lx_cmd_check(tb.workfd, cmd)
 tb.end_tc(True)
