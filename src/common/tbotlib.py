2f708bbc91805b353f883c617f3faf259b05c99d
Heiko Schocher <hs@denx.de>
2017-12-19 09:23:57 +0000

    tbotlib: add tbot_get_line() and sting_to_dict()
    add function tbot_get_line, simply get one line from
    connection.
    
    add function string_to_dict(string, pattern):
      convert a string into a dictionary via a pattern
      example pattern:
        'hello, my name is {name} and I am a {age} year old {what}'
      string:
        'hello, my name is dan and I am a 33 year old developer'
      returned dict:
        {'age': '33', 'name': 'dan', 'what': 'developer'}
      from:
        https://stackoverflow.com/questions/11844986/convert-or-unformat-a-string-to-variables-like-format-but-in-reverse-in-p
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/common/tbotlib.py
+++ src/common/tbotlib.py
@@ -1389,6 +1389,34 @@ class tbot(object):
             self.tbot_expect_prompt(c)
         return True
 
+    def tbot_get_line(self, c):
+        """ get one line, if prompt read return ''
+
+        - **parameters**, **types**, **return** and **return types**::
+        :param arg1: connection
+        """
+        ret = self.tbot_rup_and_check_strings(c, '\n')
+        if ret == 'prompt':
+            return ''
+        return self.buf
+
+    def string_to_dict(self, string, pattern):
+        """ convert a string into a dictionary via a pattern
+            example pattern:
+            'hello, my name is {name} and I am a {age} year old {what}'
+            string:
+            'hello, my name is dan and I am a 33 year old developer'
+            returned dict:
+            {'age': '33', 'name': 'dan', 'what': 'developer'}
+            from:
+            https://stackoverflow.com/questions/11844986/convert-or-unformat-a-string-to-variables-like-format-but-in-reverse-in-p
+        """
+        regex = re.sub(r'{(.+?)}', r'(?P<_\1>.+)', pattern)
+        values = list(re.search(regex, string).groups())
+        keys = re.findall(r'{(.+?)}', pattern)
+        _dict = dict(zip(keys, values))
+        return _dict
+
     def eof_write_cmd_get_line(self, c, cmd, start=True):
         """ write command and get one line back in variable ret_write_cmd_get_line
 
