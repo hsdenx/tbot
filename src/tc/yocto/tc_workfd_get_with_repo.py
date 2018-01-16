84d6a7616ee62c6bdd947a577210cf28905eb913
Heiko Schocher <hs@denx.de>
2018-01-16 11:19:41 +0000

    tc_workfd_get_with_repo.py: make more common
    make the testcase tc_workfd_get_with_repo.py more common
    usuable:
    
    - setup environment with samples from meta-
      tb.config.tc_workfd_get_with_repo_metaname
      default: 'beld'
    
    - check if build exists, if not create it and
      set DL_DIR and SSTATE_DIR in local.conf with the
      values from tb.config.tc_workfd_get_yocto_source_conf_dl_dir
      and tb.config.tc_workfd_get_yocto_source_conf_sstate_dir
    
    and setup site.conf with specific settings with
    testcases:
    
    src/tc/yocto/tc_workfd_yocto_patch_site.py
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/yocto/tc_workfd_get_with_repo.py
+++ src/tc/yocto/tc_workfd_get_with_repo.py
@@ -30,20 +30,32 @@
 #
 # - get newest sources with "repo sync"
 #
+# - setup environment with samples from meta-
+#   tb.config.tc_workfd_get_with_repo_metaname
+#   default: 'beld'
+#
 # - check if build exists, if not create it and
 #   set DL_DIR and SSTATE_DIR in local.conf with the
 #   values from tb.config.tc_workfd_get_yocto_source_conf_dl_dir
 #   and tb.config.tc_workfd_get_yocto_source_conf_sstate_dir
 #
+#   and setup site.conf with specific settings
+#
 # End:
 
 from tbotlib import tbot
+
+try:
+    tb.config.tc_workfd_get_with_repo_metaname
+except:
+    tb.config.tc_workfd_get_with_repo_metaname = 'beld'
 
 logging.info("args: workdfd: %s", tb.workfd.name)
 logging.info("args: repo u: %s", tb.config.tc_workfd_get_with_repo_u)
 logging.info("args: repo m: %s", tb.config.tc_workfd_get_with_repo_m)
 logging.info("args: repo b: %s", tb.config.tc_workfd_get_with_repo_b)
 logging.info("args: repo target: %s", tb.config.tc_workfd_get_with_repo_target)
+logging.info("args: meta name: %s", tb.config.tc_workfd_get_with_repo_metaname)
 
 # check if we have the repo cmd installed
 # if not try to set PATH (if tb.config.tc_workfd_repo_path is set)
@@ -54,36 +66,40 @@ if ret == False:
     # get sources
     # step 1: create dir
     cmd = 'mkdir -p $TBOT_BASEDIR_REPO'
-    tb.write_lx_cmd_check(tb.workfd, cmd)
+    tb.write_lx_cmd_check(tb.workfd, cmd, create_doc_event=True)
     tb.eof_call_tc("tc_workfd_goto_repo_code.py")
 
     # step 2: repo init
     cmd = 'repo init -u ' + tb.config.tc_workfd_get_with_repo_u + ' -m ' + tb.config.tc_workfd_get_with_repo_m + ' -b ' + tb.config.tc_workfd_get_with_repo_b
-    tb.write_lx_cmd_check(tb.workfd, cmd, triggerlist=['objects', 'deltas'])
+    tb.write_lx_cmd_check(tb.workfd, cmd, triggerlist=['objects', 'deltas'], create_doc_event=True)
 
 # step 3: repo sync
-tb.write_lx_cmd_check(tb.workfd, 'repo sync', triggerlist=['objects', 'deltas', 'done', 'project'])
+tb.write_lx_cmd_check(tb.workfd, 'repo sync', triggerlist=['objects', 'deltas', 'done', 'project'], create_doc_event=True)
 
 # step 4:configure project
 # goto yocto code
 tb.workfd.tc_workfd_goto_yocto_code_path = '$TBOT_BASEDIR_REPO/' + tb.config.tc_workfd_get_with_repo_target
+tb.event.create_event('main', 'tc_workfd_get_with_repo.py', 'SET_DOC_FILENAME', 'repo_goto_yocto_code')
 tb.eof_call_tc("tc_workfd_goto_yocto_code.py")
 
 # check if 'build' exists, if not configure
 tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_BASEDIR_YOCTO/build'
 ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
 if ret == False:
-    cmd = 'TEMPLATECONF=meta-beld/conf/samples/ source oe-init-build-env'
-    tb.write_lx_cmd_check(tb.workfd, cmd)
+    cmd = 'TEMPLATECONF=meta-' + tb.config.tc_workfd_get_with_repo_metaname + '/conf/samples/ source oe-init-build-env'
+    tb.write_lx_cmd_check(tb.workfd, cmd, create_doc_event=True)
     tb.eof_call_tc("tc_workfd_check_if_dir_exist.py")
 
     # step 5: adapt conf file
     if tb.config.tc_workfd_get_yocto_source_conf_dl_dir != 'none':
         cmd = "sed -i '/DL_DIR ?=/cDL_DIR=\"" + tb.config.tc_workfd_get_yocto_source_conf_dl_dir + "\"' conf/local.conf"
-        tb.write_lx_cmd_check(tb.workfd, cmd)
+        tb.write_lx_cmd_check(tb.workfd, cmd, create_doc_event=True)
 
     if tb.config.tc_workfd_get_yocto_source_conf_sstate_dir != 'none':
         cmd = "sed -i '/SSTATE_DIR ?=/cSSTATE_DIR=\"" + tb.config.tc_workfd_get_yocto_source_conf_sstate_dir  + "\"' conf/local.conf"
-        tb.write_lx_cmd_check(tb.workfd, cmd)
+        tb.write_lx_cmd_check(tb.workfd, cmd, create_doc_event=True)
+
+    # step 6: patch/create site.conf
+    tb.eof_call_tc("tc_workfd_yocto_patch_site.py")
 
 tb.end_tc(True)
