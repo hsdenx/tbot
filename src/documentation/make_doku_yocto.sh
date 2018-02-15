eea9aef07bd09f35f755c5c6bc595907921ce1e5
Heiko Schocher <hs@denx.de>
2018-02-15 11:51:11 +0000

    make_doku_yocto.sh: introduce logfilespath var
    introduce logfilespath variable, also rename
    filenames with "cpc" into "ctrl"
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/documentation/make_doku_yocto.sh
+++ src/documentation/make_doku_yocto.sh
@@ -1,7 +1,9 @@
 wdir="/home/pi/tbot2go/tbot/src/documentation"
 srcpath="/home/pi/tbot2go/documentation/yocto"
+logfilespath="/home/pi/tbot2go/documentation/yocto/logfiles_get_and_bake/"
+logfilespath="/home/pi/tbot2go/documentation/yocto/logfiles_bbb/"
 
-for f in $srcpath/logfiles_get_and_bake/*.txt;
+for f in $logfilespath*.txt;
 do
   # echo "Processing $f file..";
   # check if it contains escape sequences
@@ -13,7 +15,18 @@ do
   fi
 done
 
-python2.7 $wdir/replace_tbot_marker.py -i $srcpath/index.rst -o $srcpath/work/index.rst -t $srcpath/logfiles_get_and_bake/ -r True -l rst -w 125
+for f in $logfilespath*.txt;
+do
+    found=`echo $f | grep -c cpc`
+    if [ $found -ne 0 ]; then
+        echo "FOUND " $f
+        n=${f/cpc/ctrl}
+        echo "FOUND " $n
+        mv $f $n
+    fi
+done
+
+python2.7 $wdir/replace_tbot_marker.py -i $srcpath/index.rst -o $srcpath/work/index.rst -t $logfilespath -r True -l rst -w 125
 rst2pdf -s $wdir/stylesheet.txt $srcpath/work/index.rst $srcpath/pdf/index.pdf
 python2.7 $wdir/mark_red.py -i $srcpath/pdf/index.pdf -o $srcpath/pdf/yocto_bbb.pdf
 
