86cf2af36f60e55fe94183d5a4127bca38d26b51
Heiko Schocher <hs@denx.de>
2018-05-07 07:39:01 +0000

    yocto: tc_workfd_yocto_patch_site.py: use tb.config.builddir
    use tb.config.builddir so yocto build directory is
    configurable.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/yocto/tc_workfd_yocto_patch_site.py
+++ src/tc/yocto/tc_workfd_yocto_patch_site.py
@@ -81,12 +81,17 @@ try:
 except:
     tb.config.tc_workfd_yocto_patch_site_ub_key = 'none'
 
+try:
+    tb.config.builddir
+except:
+    tb.config.builddir = "$TBOT_BASEDIR_YOCTO/build/"
+
 logging.info("args: workdfd: %s", tb.workfd.name)
 logging.info("args: default filepath: %s", tb.config.tc_workfd_yocto_patch_site_path_default_file)
 
 tb.eof_call_tc("tc_workfd_goto_yocto_code.py")
 tb.event.create_event('main', 'tc_workfd_get_with_repo.py', 'SET_DOC_FILENAME_NOIRQ', 'repo_create_site.conf')
-p = '$TBOT_BASEDIR_YOCTO/build'
+p = tb.config.builddir
 tb.config.tc_workfd_check_if_dir_exists_name = p
 tb.eof_call_tc("tc_workfd_check_if_dir_exist.py")
 
