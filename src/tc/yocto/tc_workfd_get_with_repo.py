9b301ab01bf3f6d229c45859e0a6a2097855b725
Heiko Schocher <hs@denx.de>
2018-05-07 07:37:20 +0000

    yocto: tc_workfd_get_with_repo.py: build directory configurable
    yocto build directoy is now configurable:
    
    "build_" + tb.config.tc_workfd_bitbake_machine
    
    and set tb.config.builddir
    
    tb.config.tc_workfd_bitbake_machine default is tb.config.boardname
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/yocto/tc_workfd_get_with_repo.py
+++ src/tc/yocto/tc_workfd_get_with_repo.py
@@ -34,9 +34,9 @@
 #   tb.config.tc_workfd_get_with_repo_metaname
 #   default: 'beld'
 #
-# - check if build exists, if not create it and
-#   set DL_DIR and SSTATE_DIR in local.conf with the
-#   values from tb.config.tc_workfd_get_yocto_source_conf_dl_dir
+# - check if build directory "build_" + tb.config.tc_workfd_bitbake_machine
+#   exists, if not create it and set DL_DIR and SSTATE_DIR in local.conf
+#   with the values from tb.config.tc_workfd_get_yocto_source_conf_dl_dir
 #   and tb.config.tc_workfd_get_yocto_source_conf_sstate_dir
 #
 #   and setup site.conf with specific settings
@@ -50,12 +50,24 @@ try:
 except:
     tb.config.tc_workfd_get_with_repo_metaname = 'beld'
 
+try:
+    tb.config.tc_workfd_bitbake_machine
+except:
+    tb.config.tc_workfd_bitbake_machine = tb.config.boardname
+
+try:
+    tb.config.builddir
+except:
+    tb.config.builddir = "$TBOT_BASEDIR_YOCTO/build_" + tb.config.tc_workfd_bitbake_machine + "/"
+
 logging.info("args: workdfd: %s", tb.workfd.name)
 logging.info("args: repo u: %s", tb.config.tc_workfd_get_with_repo_u)
 logging.info("args: repo m: %s", tb.config.tc_workfd_get_with_repo_m)
 logging.info("args: repo b: %s", tb.config.tc_workfd_get_with_repo_b)
 logging.info("args: repo target: %s", tb.config.tc_workfd_get_with_repo_target)
 logging.info("args: meta name: %s", tb.config.tc_workfd_get_with_repo_metaname)
+logging.info("args: builddir: %s", tb.config.builddir)
+logging.info("args: machine: %s", tb.config.tc_workfd_bitbake_machine)
 
 # check if we have the repo cmd installed
 # if not try to set PATH (if tb.config.tc_workfd_repo_path is set)
@@ -83,12 +95,12 @@ tb.workfd.tc_workfd_goto_yocto_code_path = '$TBOT_BASEDIR_REPO/' + tb.config.tc_
 tb.event.create_event('main', 'tc_workfd_get_with_repo.py', 'SET_DOC_FILENAME', 'repo_goto_yocto_code')
 tb.eof_call_tc("tc_workfd_goto_yocto_code.py")
 
-# check if 'build' exists, if not configure
-tb.config.tc_workfd_check_if_dir_exists_name = '$TBOT_BASEDIR_YOCTO/build'
+# check if builddir exists, if not configure
+tb.config.tc_workfd_check_if_dir_exists_name = tb.config.builddir
 ret = tb.call_tc("tc_workfd_check_if_dir_exist.py")
 if ret == False:
     tb.event.create_event('main', 'tc_workfd_get_with_repo.py', 'SET_DOC_FILENAME', 'repo_templateconf')
-    cmd = 'TEMPLATECONF=meta-' + tb.config.tc_workfd_get_with_repo_metaname + '/conf/samples/ source oe-init-build-env'
+    cmd = 'TEMPLATECONF=meta-' + tb.config.tc_workfd_get_with_repo_metaname + '/conf/samples/ source oe-init-build-env ' + 'build_' + tb.config.tc_workfd_bitbake_machine
     tb.write_lx_cmd_check(tb.workfd, cmd)
     tb.eof_call_tc("tc_workfd_check_if_dir_exist.py")
 
@@ -108,7 +120,7 @@ if ret == False:
 else:
     # print current used site.conf
     tb.event.create_event('main', 'tc_workfd_get_with_repo.py', 'SET_DOC_FILENAME_NOIRQ', 'repo_dump_site.conf')
-    tb.write_lx_cmd_check(tb.workfd, "cat " + fn)
+    tb.write_lx_cmd_check(tb.workfd, "cat " + tb.config.builddir + "conf/site.conf")
     tb.event.create_event('main', 'tc_workfd_get_with_repo.py', 'SET_DOC_FILENAME_NOIRQ_END', 'repo_dump_site.conf')
 
 tb.end_tc(True)
