ae05b22c354d7150b51caa4be932707ce5aead96
Heiko Schocher <hs@denx.de>
2018-05-25 05:26:04 +0000

    tc_workfd_grep.py: split lines > 100 chars
    split lines which are longer than 100 chars.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_grep.py
+++ src/tc/linux/tc_workfd_grep.py
@@ -31,6 +31,6 @@ logging.info("args: workfd: %s %s %s", tb.workfd.name, tb.tc_workfd_grep_file, t
 c = tb.workfd
 
 cmd = 'cat ' + tb.tc_workfd_grep_file + ' | grep ' + tb.config.tc_workfd_grep_option + ' ' + tb.tc_workfd_grep_string
-tb.write_lx_cmd_check(c, cmd)
+tb.write_lx_cmd_check(c, cmd, split=100)
 
 tb.end_tc(True)
