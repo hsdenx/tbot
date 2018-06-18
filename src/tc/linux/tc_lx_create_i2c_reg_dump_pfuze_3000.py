1c6cc9e0448d81b81ff5825f82275107c2a4fcf1
Heiko Schocher <hs@denx.de>
2018-06-18 12:01:22 +0000

    new testcase: tc_lx_create_i2c_reg_dump_pfuze_3000.py
    create with i2cget tool a register dump of
    pfuze3000 on bus
    tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

---
+++ src/tc/linux/tc_lx_create_i2c_reg_dump_pfuze_3000.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# Description:
+# create with i2cget tool a register dump of
+# pfuze3000 on bus
+# tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus
+#
+# used variables:
+#
+# - tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus
+#   i2c bus number on which the pfuze3000 is connected to.
+#   default: '0x0'
+#
+# End:
+
+from tbotlib import tbot
+
+try:
+    tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus
+except:
+    tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus = '0x0'
+
+logging.info("args: bus: %s", tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus)
+
+tb.config.tc_lx_create_i2c_reg_file_bus = tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus
+tb.config.tc_lx_create_i2c_reg_file_addr = '0x08'
+tb.config.tc_lx_create_i2c_reg_file_start = '0x00'
+tb.config.tc_lx_create_i2c_reg_file_stop = '0x70'
+tb.config.tc_lx_create_i2c_reg_file_name = 'i2c_dump_pfuze3000@' + tb.config.tc_lx_create_i2c_reg_dump_pfuze_3000_bus + '.txt'
+tb.eof_call_tc("tc_lx_create_i2c_reg_file.py")
+
+tb.end_tc(True)
