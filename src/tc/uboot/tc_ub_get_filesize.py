e2e513a0812a377e017679235fd3b8f1947f1850
Heiko Schocher <hs@denx.de>
2018-06-30 19:23:47 +0200

    tc_ub_get_filesize.py: rework testcase documentation
    rework testcase documentation
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_ub_get_filesize.py
+++ src/tc/uboot/tc_ub_get_filesize.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_ub_get_filesize.py
 # simple get the content of U-Boot env variable filesize
 # and store it in tb.ub_filesize
 # End:
