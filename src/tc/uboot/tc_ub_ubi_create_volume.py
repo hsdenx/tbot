b1d4dd04a1ce10ffe25ba58abc7571a9956b9a89
Heiko Schocher <hs@denx.de>
2018-06-30 20:23:17 +0200

    tc_ub_ubi_create_volume.py: documentation and default vars update
    move default vars setting to testcase and while
    at it, update documentation
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_ub_ubi_create_volume.py
+++ src/tc/uboot/tc_ub_ubi_create_volume.py
@@ -1,15 +1,25 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_ub_ubi_create_volume.py
-# - create ubi volume tb.config.tc_ub_ubi_create_vol_name with size
+# create ubi volume tb.config.tc_ub_ubi_create_vol_name with size
 # tb.config.tc_ub_ubi_create_vol_sz
+#
+# used variables
+#
+# - tb.config.tc_ub_ubi_create_vol_name
+#| volume name, which get created
+#| default: tb.config.tc_ub_ubi_load_name
+#
+# - tb.config.tc_ub_ubi_create_vol_sz
+#| size of volume which get created
+#| default: '600000'
+#
 # End:
 
 from tbotlib import tbot
 
-logging.info("args: %s %s", tb.config.tc_ub_ubi_create_vol_name, tb.config.tc_ub_ubi_create_vol_sz)
+tb.define_variable('tc_ub_ubi_create_vol_name', tb.config.tc_ub_ubi_load_name)
+tb.define_variable('tc_ub_ubi_create_vol_sz', '600000')
 
 # set board state for which the tc is valid
 tb.set_board_state("u-boot")
