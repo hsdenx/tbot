c899c577bc752ba858ea6933e2dd32443269f1eb
Heiko Schocher <hs@denx.de>
2018-06-30 19:54:13 +0200

    tc_ub_load_board_env.py: documentation and default vars update
    move default vars setting to testcase and while
    at it, update documentation
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_ub_load_board_env.py
+++ src/tc/uboot/tc_ub_load_board_env.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_ub_load_board_env.py
 #
 # task: load U-Boot Environment env.txt file with tftp for the
 # board tb.config.tftpboardname to the addr tb.config.ub_load_board_env_addr
@@ -20,29 +18,57 @@
 #
 # else TC executes the steps described in 'task'
 #
-# tb.config.ub_load_board_env_testcase != ''
+# tb.config.ub_load_board_env_testcase != 'none'
 # call a board specific testcase, whichs name is defined in
 # tb.config.ub_load_board_env_testcase for setting U-Boot
 # Env. If this testcase succeeds, end True.
+#
+# used variables
+#
+# - tb.config.ub_load_board_env_testcase
+#| if != 'none' call testcase with this name for
+#| for setting U-Boot Environment
+#| default: 'none'
+#
+# - tb.config.ub_load_board_env_addr
+#| ram address to which the env.txt file gets loaded
+#| default: '0x81000000'
+#
+# - tb.config.ub_load_board_env_subdir
+#| subdir name in which env.txt file is found
+#| default: 'tbot'
+#
+# - tb.config.tc_ub_boot_linux_load_env
+#| value
+#| 'no'      no Environment gets loaded
+#| 'set'     load Environment from tb.config.ub_load_board_env_set
+#| 'setend'  same as 'set' just end testcase after
+#|           Environment is set from tb.config.ub_load_board_env_set
+#| 'load'    tftp a 'env.txt' file and import it with
+#|           'env import -t'
+#| default: 'load'
+#
+# - tb.config.ub_load_board_env_set
+#| list of Environment settings
+#| default: '[]'
+#
 # End:
 
 from tbotlib import tbot
 
-try:
-    tb.config.ub_load_board_env_testcase
-except:
-    tb.config.ub_load_board_env_testcase = ''
+tb.define_variable('ub_load_board_env_testcase', 'none')
+tb.define_variable('ub_load_board_env_addr', '0x81000000')
+tb.define_variable('ub_load_board_env_subdir', 'tbot')
+tb.define_variable('tc_ub_boot_linux_load_env', 'load')
+tb.define_variable('ub_load_board_env_set', '[]')
 
-logging.info("args: %s %s %s", tb.config.tftpboardname, tb.config.ub_load_board_env_addr, tb.config.ub_load_board_env_subdir)
-logging.info("args: %s", tb.config.tc_ub_boot_linux_load_env)
+logging.info("args: %s", tb.config.tftpboardname)
 logging.info("args: %s", tb.config.tftpboardrootdir)
-logging.info("args: %s", tb.config.ub_load_board_env_set)
-logging.info("args: %s", tb.config.ub_load_board_env_testcase)
 
 # set board state for which the tc is valid
 tb.set_board_state("u-boot")
 
-if tb.config.ub_load_board_env_testcase != '':
+if tb.config.ub_load_board_env_testcase != 'none':
     tb.eof_call_tc(tb.config.ub_load_board_env_testcase)
     tb.end_tc(True)
 
