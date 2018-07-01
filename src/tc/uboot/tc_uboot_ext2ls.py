46e76da4705bc5f3416fa847ad7dff47ee5cbe76
Heiko Schocher <hs@denx.de>
2018-07-01 09:33:57 +0200

    tc_uboot_ext2ls.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_uboot_ext2ls.py
+++ src/tc/uboot/tc_uboot_ext2ls.py
@@ -1,46 +1,39 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s lab_denx -c fipad -t tc_uboot_ext2ls.py
 #
-# simply call ext2ls
+# simply call ext2ls U-Boot command and check if
+# all strings in tb.config.tc_uboot_ext2ls_expect
+# come back from the command.
 #
-# used vars:
-# tb.config.tc_uboot_ext2ls_expect = ['lost+found']
-#   strings we expect from the ext2ls command
-# tb.config.tc_uboot_ext2ls_interface = 'usb'
-# tb.config.tc_uboot_ext2ls_dev = '0:1'
-# tb.config.tc_uboot_ext2ls_dir = '/'
+# used variables
+#
+# - tb.config.tc_uboot_ext2ls_expect
+#| list of strings, which should come back from
+#| command 'ext2ls'
+#| default: "['lost+found']"
+#
+# - tb.config.tc_uboot_ext2ls_interface
+#| used interface for ext2ls command
+#| default: 'usb'
+#
+# - tb.config.tc_uboot_ext2ls_dev
+#| device used for ext2ls command
+#| default: '0:1'
+#
+# - tb.config.tc_uboot_ext2ls_dir
+#| directory, which gets dumped
+#| default: '/'
+#
 # End:
 
 from tbotlib import tbot
 
-try:
-    tb.config.tc_uboot_ext2ls_expect
-except:
-    tb.config.tc_uboot_ext2ls_expect = [
-    'lost+found']
+tb.define_variable('tc_uboot_ext2ls_expect', "['lost+found']")
+tb.define_variable('tc_uboot_ext2ls_interface', 'usb')
+tb.define_variable('tc_uboot_ext2ls_dev', '0:1')
+tb.define_variable('tc_uboot_ext2ls_dir', '/')
 
-try:
-    tb.config.tc_uboot_ext2ls_interface
-except:
-    tb.config.tc_uboot_ext2ls_interface = 'usb'
-
-try:
-    tb.config.tc_uboot_ext2ls_dev
-except:
-    tb.config.tc_uboot_ext2ls_dev = '0:1'
-
-try:
-    tb.config.tc_uboot_ext2ls_dir
-except:
-    tb.config.tc_uboot_ext2ls_dir = '/'
-
-# here starts the real test
-logging.info("testcase arg: %s %s %s %s", tb.config.tc_uboot_ext2ls_expect,
-  tb.config.tc_uboot_ext2ls_interface, tb.config.tc_uboot_ext2ls_dev,
-  tb.config.tc_uboot_ext2ls_dir)
 # set board state for which the tc is valid
 tb.set_board_state("u-boot")
 
