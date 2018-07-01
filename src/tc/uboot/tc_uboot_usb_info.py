474c08c6a9b349203205058dfbb210a3c8277828
Heiko Schocher <hs@denx.de>
2018-07-01 10:14:33 +0200

    tc_uboot_usb_info.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_uboot_usb_info.py
+++ src/tc/uboot/tc_uboot_usb_info.py
@@ -1,26 +1,21 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s lab_denx -c fipad -t tc_uboot_usb_info.py
 #
 # call "usb info" command
 #
-# used vars:
-# tb.config.tc_uboot_usb_info_expect = ['Hub,  USB Revision 2.0',
-#    'Mass Storage,  USB Revision 2.0']
+# used variables:
+#
+# - tb.config.tc_uboot_usb_info_expect
+#| strings which must come from "usb info" command
+#| default: "['Hub,  USB Revision 2.0', 'Mass Storage,  USB Revision 2.0']"
+#
 # End:
 
 from tbotlib import tbot
 
-try:
-    tb.config.tc_uboot_usb_info_expect
-except:
-    tb.config.tc_uboot_usb_info_expect = ['Hub,  USB Revision 2.0',
-    'Mass Storage,  USB Revision 2.0']
+tb.define_variable('tc_uboot_usb_info_expect', "['Hub,  USB Revision 2.0', 'Mass Storage,  USB Revision 2.0']")
 
-# here starts the real test
-logging.info("testcase arg: %s", tb.config.tc_uboot_usb_info_expect)
 # set board state for which the tc is valid
 tb.set_board_state("u-boot")
 
