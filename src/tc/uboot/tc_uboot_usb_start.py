1faf69b650c2eba3d0c13d7abb0167ed2cef765a
Heiko Schocher <hs@denx.de>
2018-07-01 10:16:18 +0200

    tc_uboot_usb_start.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_uboot_usb_start.py
+++ src/tc/uboot/tc_uboot_usb_start.py
@@ -1,24 +1,21 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s lab_denx -c fipad -t tc_uboot_usb_start.py
 #
 # call "usb start" command
 #
-# used vars:
-# tb.config.tc_uboot_usb_start_expect = ['Storage Device(s) found']
+# used variable
+#
+# - tb.config.tc_uboot_usb_start_expect
+#| strings which must come back from "usb start" command
+#| default: 'Storage Device(s) found'
+#
 # End:
 
 from tbotlib import tbot
 
-try:
-    tb.config.tc_uboot_usb_start_expect
-except:
-    tb.config.tc_uboot_usb_start_expect = ['Storage Device(s) found']
+tb.define_variable('tc_uboot_usb_start_expect', 'Storage Device(s) found')
 
-# here starts the real test
-logging.info("testcase arg: %s", tb.config.tc_uboot_usb_start_expect)
 # set board state for which the tc is valid
 tb.set_board_state("u-boot")
 
