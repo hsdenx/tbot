a669be8e2a9f23e3e6674456e739d93e953825ec
Heiko Schocher <hs@denx.de>
2018-07-01 09:00:09 +0200

    tc_ub_ubifs_ls.py: documentation and default vars update
    move default vars setting to testcase and while
    at it, update documentation
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/uboot/tc_ub_ubifs_ls.py
+++ src/tc/uboot/tc_ub_ubifs_ls.py
@@ -1,14 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_ub_ubifs_ls.py
-# - ls ubifs tb.config.tc_ub_ubifs_ls_dir
+# ls ubifs tb.config.tc_ub_ubifs_ls_dir
+#
+# used variables
+#
+# - tb.config.tc_ub_ubifs_ls_dir
+#| directory path which gets listed with 'ubifsls'
+#| default: '/'
+#
 # End:
 
 from tbotlib import tbot
 
-logging.info("args: %s", tb.config.tc_ub_ubifs_ls_dir)
+tb.define_variable('tc_ub_ubifs_ls_dir', '/')
 
 # set board state for which the tc is valid
 tb.set_board_state("u-boot")
