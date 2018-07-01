8d7dee8d32857f4f47f0ed43ea385bf11aca3ebe
Heiko Schocher <hs@denx.de>
2018-07-01 11:51:45 +0200

    tc_workfd_check_repo_cmd.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/yocto/tc_workfd_check_repo_cmd.py
+++ src/tc/yocto/tc_workfd_check_repo_cmd.py
@@ -1,28 +1,28 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_check_repo_cmd.py
 #
 # check if repo cmd exists, if not try to set
 # PATH variable stored in tb.config.tc_workfd_repo_path
+#
+# used variable:
+#
+# - tb.config.tc_workfd_repo_path
+#| PATH to repo command. If 'repo' command is not found
+#| add this path to PATH Environment variable.
+#| default: 'none'
 #
 # End:
 
 from tbotlib import tbot
 
+tb.define_variable('tc_workfd_repo_path', 'none')
 try:
     tb.config.tc_workfd_repo_cmd_checked
 except:
     tb.config.tc_workfd_repo_cmd_checked = False
 
-try:
-    tb.config.tc_workfd_repo_path
-except:
-    tb.config.tc_workfd_repo_path = ''
-
 logging.info("args: %s %s", tb.workfd, tb.config.tc_workfd_repo_cmd_checked)
-logging.info("args: %s", tb.config.tc_workfd_repo_path)
 
 if tb.config.tc_workfd_repo_cmd_checked == False:
     tb.config.tc_workfd_check_if_cmd_exist_cmdname = 'repo'
@@ -32,7 +32,7 @@ if tb.config.tc_workfd_repo_cmd_checked == False:
         tb.end_tc(True)
 
     # repo not found
-    if tb.config.tc_workfd_repo_path == '':
+    if tb.config.tc_workfd_repo_path == 'none':
         tb.end_tc(False)
     else:
         # set path stored in tb.config.tc_workfd_repo_path
