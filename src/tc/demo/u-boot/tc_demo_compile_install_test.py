5d980906a3be76a61786b8c8442337a8d56b751e
Heiko Schocher <hs@denx.de>
2018-07-02 12:10:45 +0200

    tc_demo_compile_install_test.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/demo/u-boot/tc_demo_compile_install_test.py
+++ src/tc/demo/u-boot/tc_demo_compile_install_test.py
@@ -1,79 +1,82 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# tbot.py -c -s lab_denx -c demo -t tc_demo_compile_install_test.py
-# start tc:
 # - if tb.config.tc_board_bootmode_tc is defined
-#   call tc tb.config.tc_board_bootmode_tc
-#   (set bootmode for the board)
+#|   call tc tb.config.tc_board_bootmode_tc
+#|   (set bootmode for the board)
 # - go to uboot code with tc_workfd_goto_uboot_code.py
 # - set toolchain with tc_workfd_set_toolchain.py
 # - compile source tree with tc_workfd_compile_uboot.py
 # - if tb.config.tc_demo_uboot_test_deploy != ''
-#     call tb.config.tc_demo_uboot_test_deploy
-#   else
-#     copy files in list tb.config.tc_demo_compile_install_test_files
-#     tb.config.tc_demo_compile_install_test_files contains a list of files,
-#     which are copied to tftp directory
-#     tb.config.tftpdir + tb.config.tftpboardname + '/' + tb.config.ub_load_board_env_subdir
+#|     call tb.config.tc_demo_uboot_test_deploy
+#|   else
+#|     copy files in list tb.config.tc_demo_compile_install_test_files
+#|     tb.config.tc_demo_compile_install_test_files contains a list of files,
+#|     which are copied to tftp directory
+#|     tb.config.tftpdir + tb.config.tftpboardname + '/' + tb.config.ub_load_board_env_subdir
 # - get u-boot version from binary with tc_ub_get_version.py
 # - if tb.config.tc_demo_uboot_test_update != '':
-#     call tb.config.tc_demo_uboot_test_update
-#   else:
-#     call tc_ub_upd_uboot.py
-#     call tc_ub_upd_spl.py
+#|     call tb.config.tc_demo_uboot_test_update
+#|   else:
+#|     call tc_ub_upd_uboot.py
+#|     call tc_ub_upd_spl.py
 # - if tb.config.tc_demo_compile_install_test_spl_vers_file and/or
-#   tc_tb.config.demo_compile_install_test_ub_vers_file != ''
-#   check if the new installed version is the same
-#   as in the binary files, defined in
-#   tb.config.tc_demo_compile_install_test_ub_vers_file or
-#   tb.config.tc_demo_compile_install_test_spl_vers_file
+#|   tc_tb.config.demo_compile_install_test_ub_vers_file != ''
+#|   check if the new installed version is the same
+#|   as in the binary files, defined in
+#|   tb.config.tc_demo_compile_install_test_ub_vers_file or
+#|   tb.config.tc_demo_compile_install_test_spl_vers_file
 # - call tb.config.tc_demo_compile_install_test_name
-#     which should contain a testcase, which tests the new
-#     installed u-boot
+#|     which should contain a testcase, which tests the new
+#|     installed u-boot
 # - if tb.config.tc_demo_compile_install_test_poweroff == 'yes':
-#     power off board at the end.
+#|     power off board at the end.
+#
+# used variables:
+#
+# - tb.config.tc_demo_compile_install_test_ub_vers_file
+#| if != 'none' contains the filename, in which testcase
+#| tc_ub_get_version.py find the U-Boot version
+#| default: 'none'
+#
+# - tb.config.tc_demo_compile_install_test_spl_vers_file
+#| if != 'none' contains the filename, in which testcase
+#| tc_ub_get_version.py find the U-Boot SPL version
+#| default: 'none'
+#
+# - tb.config.tc_demo_uboot_test_deploy
+#| if != 'none' call testcase with name tb.config.tc_demo_uboot_test_deploy
+#| for installing U-Boot
+#| default: 'none'
+#
+# - tb.config.tc_board_bootmode_tc
+#| if != 'none' call testcase with name tb.config.tc_board_bootmode_tc
+#| for switching bootmode of the board
+#| default: 'none'
+#
+# - tb.config.tc_demo_compile_install_test_poweroff
+#| if 'yes, switch off board power after successful test
+#| default: 'yes'
+#
+# - tb.config.tc_demo_compile_install_test_name
+#| if != 'none' call testcase with name tb.config.tc_demo_compile_install_test_name
+#| to test the new installed u-boot
+#| default: 'none'
+#
 # End:
 
 from tbotlib import tbot
 
-try:
-    tb.config.tc_demo_compile_install_test_ub_vers_file
-except:
-    tb.config.tc_demo_compile_install_test_ub_vers_file = ''
+tb.define_variable('tc_demo_compile_install_test_ub_vers_file', 'none')
+tb.define_variable('tc_demo_compile_install_test_spl_vers_file', 'none')
+tb.define_variable('tc_demo_uboot_test_deploy', 'none')
+tb.define_variable('tc_board_bootmode_tc', 'none')
+tb.define_variable('tc_demo_compile_install_test_poweroff', 'yes')
+tb.define_variable('tc_demo_compile_install_test_name', 'tc_ub_help.py')
 
-try:
-    tb.config.tc_demo_compile_install_test_spl_vers_file
-except:
-    tb.config.tc_demo_compile_install_test_spl_vers_file = ''
+logging.info("args: %s", tb.workfd.name)
 
-try:
-    tb.config.tc_demo_uboot_test_deploy
-except:
-    tb.config.tc_demo_uboot_test_deploy = ''
-
-try:
-    tb.config.tc_demo_uboot_test_update
-except:
-    tb.config.tc_demo_uboot_test_update = ''
-
-try:
-    tb.config.tc_board_bootmode_tc
-except:
-    tb.config.tc_board_bootmode_tc = ''
-
-try:
-    tb.config.tc_demo_compile_install_test_poweroff
-except:
-    tb.config.tc_demo_compile_install_test_poweroff = 'yes'
-
-logging.info("args: %s %s %s %s", tb.workfd.name, tb.config.tc_demo_uboot_test_deploy,
-             tb.config.tc_demo_uboot_test_update, tb.config.tc_board_bootmode_tc)
-logging.info("args: %s", tb.config.tc_demo_compile_install_test_name)
-logging.info("args: %s", tb.config.tc_demo_compile_install_test_poweroff)
-
-if tb.config.tc_board_bootmode_tc != '':
+if tb.config.tc_board_bootmode_tc != 'none':
     tb.config.tc_board_bootmode = 'normal'
     tb.eof_call_tc(tb.config.tc_board_bootmode_tc)
 
@@ -89,7 +92,7 @@ tb.statusprint("compile u-boot")
 tb.eof_call_tc("tc_workfd_compile_uboot.py")
 
 r = tb.config.tftpdir + '/'
-if tb.config.tc_demo_uboot_test_deploy != '':
+if tb.config.tc_demo_uboot_test_deploy != 'none':
     tb.eof_call_tc(tb.config.tc_demo_uboot_test_deploy)
 else:
     # copy files to tbot dir
@@ -103,7 +106,7 @@ else:
 tb.eof_call_tc("tc_workfd_goto_uboot_code.py")
 
 # check U-Boot version
-if tb.config.tc_demo_compile_install_test_ub_vers_file != '':
+if tb.config.tc_demo_compile_install_test_ub_vers_file != 'none':
     tb.config.tc_ub_get_version_file = tb.config.tc_demo_compile_install_test_ub_vers_file
     tb.config.tc_ub_get_version_string = 'U-Boot 20'
     tb.eof_call_tc("tc_ub_get_version.py")
@@ -111,7 +114,7 @@ if tb.config.tc_demo_compile_install_test_ub_vers_file != '':
 else:
     tb.uboot_vers = ''
 
-if tb.config.tc_demo_compile_install_test_spl_vers_file != '':
+if tb.config.tc_demo_compile_install_test_spl_vers_file != 'none':
     tb.config.tc_ub_get_version_file = tb.config.tc_demo_compile_install_test_spl_vers_file
     tb.config.tc_ub_get_version_string = 'U-Boot SPL'
     tb.eof_call_tc("tc_ub_get_version.py")
@@ -119,7 +122,7 @@ if tb.config.tc_demo_compile_install_test_spl_vers_file != '':
 else:
     tb.spl_vers = ''
 
-if tb.config.tc_demo_uboot_test_update != '':
+if tb.config.tc_demo_uboot_test_update != 'none':
     tb.eof_call_tc(tb.config.tc_demo_uboot_test_update)
 else:
     # call upd_uboot
@@ -133,9 +136,10 @@ tb.eof_call_tc("tc_ub_check_version.py")
 
 # call tc_help
 tb.statusprint("start u-boot tests")
-tb.eof_call_tc(tb.config.tc_demo_compile_install_test_name)
+if tb.config.tc_demo_compile_install_test_name != 'none':
+    tb.eof_call_tc(tb.config.tc_demo_compile_install_test_name)
 
-if tb.config.tc_demo_uboot_test_deploy == '':
+if tb.config.tc_demo_uboot_test_deploy == 'none':
     # save working u-boot bin
     c = tb.c_ctrl
     p = tb.config.tftpdir + tb.config.tftpboardname + "/" + tb.config.ub_load_board_env_subdir + "/"
