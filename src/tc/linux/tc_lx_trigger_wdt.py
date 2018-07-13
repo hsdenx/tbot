4854591741d13000e555d891fd85b8610e8bda79
Heiko Schocher <hs@denx.de>
2018-07-13 12:17:06 +0200

    tc_lx_trigger_wdt.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_lx_trigger_wdt.py
+++ src/tc/linux/tc_lx_trigger_wdt.py
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_lx_trigger_wdt.py
 # simple trigger wdt with command tb.config.tc_lx_trigger_wdt_cmd
+#
+# used variables
+#
+# - tb.config.tc_lx_trigger_wdt_cmd
+#| command with which wdt gets triggered.
+#| default: '/home/hs/wdt &'
+#
 # End:
 
 from tbotlib import tbot
 import re
 
-logging.info("args: %s", tb.config.tc_lx_trigger_wdt_cmd)
+tb.define_variable('tc_lx_trigger_wdt_cmd', '/home/hs/wdt &')
 
 # set board state for which the tc is valid
 tb.set_board_state("linux")
