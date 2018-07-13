0e43342bbfa9607be1636fd1c12fc682c1c7ed0a
Heiko Schocher <hs@denx.de>
2018-07-13 15:13:13 +0200

    tc_workfd_create_ubi_rootfs.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_create_ubi_rootfs.py
+++ src/tc/linux/tc_workfd_create_ubi_rootfs.py
@@ -1,19 +1,30 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_create_ubi_rootfs.py
 # create a ubifs rootfs
 # ubi rootfs path: tb.config.tc_workfd_create_ubi_rootfs_path
 # ubi parameters:
 # tb.config.tc_ubi_min_io_size tb.config.tc_ubi_leb_size tb.config.tc_ubi_max_leb_cnt
 # output path: tb.config.tc_workfd_create_ubi_rootfs_target
+#
+# used variables
+#
+# - tb.config.tc_workfd_create_ubi_rootfs_path
+#| path into which the ubifs image with name
+#| tb.config.tc_workfd_create_ubi_rootfs_target get created
+#| default: '/opt/eldk-5.4/armv7a-hf/rootfs-minimal-mtdutils'
+#
+# - tb.config.tc_workfd_create_ubi_rootfs_target
+#| name of the ubi image which get created
+#| default: '/tftpboot/dxr2/tbot/rootfs-minimal.ubifs'
+#
 # End:
 from tbotlib import tbot
 
-logging.info("args: workdfd: %s %s", tb.workfd.name, tb.config.tc_workfd_create_ubi_rootfs_path)
-logging.info("%s", tb.config.tc_workfd_create_ubi_rootfs_target)
-
+tb.define_variable('tc_workfd_create_ubi_rootfs_path', '/opt/eldk-5.4/armv7a-hf/rootfs-minimal-mtdutils')
+tb.define_variable('tc_workfd_create_ubi_rootfs_target', '/tftpboot/dxr2/tbot/rootfs-minimal.ubifs')
+logging.info("args: workdfd: %s", tb.workfd.name)
+logging.info("args: %s %s %s", tb.config.tc_ubi_min_io_size, tb.config.tc_ubi_leb_size, tb.config.tc_ubi_max_leb_cnt)
 tb.eof_call_tc("tc_workfd_switch_su.py")
 
 tmp = "date > " + tb.config.tc_workfd_create_ubi_rootfs_path + "/creation_time"
