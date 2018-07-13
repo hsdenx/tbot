79c151f173e7962e68b5864561f600c7c47778cd
Heiko Schocher <hs@denx.de>
2018-07-13 12:17:40 +0200

    tc_lx_uname.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_lx_uname.py
+++ src/tc/linux/tc_lx_uname.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_lx_uname.py
 # simple linux "uname -a" command
 # End:
 
