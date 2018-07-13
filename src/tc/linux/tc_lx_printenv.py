88e44d4f1ef1d215efa64c97998eff3e2e086fc0
Heiko Schocher <hs@denx.de>
2018-07-13 12:09:34 +0200

    tc_lx_printenv.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_lx_printenv.py
+++ src/tc/linux/tc_lx_printenv.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_lx_printenv.py
 # simple printenv linux command
 # End:
 
