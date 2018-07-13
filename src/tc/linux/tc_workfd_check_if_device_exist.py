f4fe306ec183c3e4602f1b6cab838914506996b9
Heiko Schocher <hs@denx.de>
2018-07-13 12:49:46 +0200

    tc_workfd_check_if_device_exist.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_check_if_device_exist.py
+++ src/tc/linux/tc_workfd_check_if_device_exist.py
@@ -1,17 +1,23 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_check_if_device_exist.py
 # check if a device tb.config.tc_workfd_check_if_device_exists_name exist
 # this tc returns always true, but sets
 # tb.config.tc_return True or False, because we may not
 # want to end testcase failed, if device not exists.
+#
+# used variables
+#
+# - tb.config.tc_workfd_check_if_device_exists_name
+#| device name
+#| default: ''
+#
 # End:
 
 from tbotlib import tbot
 
-logging.info("args: workfd %s %s", tb.workfd, tb.config.tc_workfd_check_if_device_exists_name)
+tb.define_variable('tc_workfd_check_if_device_exists_name', '')
+logging.info("args: workfd %s", tb.workfd)
 
 tmp = 'test -c ' + tb.config.tc_workfd_check_if_device_exists_name
 tb.write_lx_cmd_check(tb.workfd, tmp)
