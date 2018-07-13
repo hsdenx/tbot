5f33dae431dfa18615ff7dbb0e427f0d0d9121fc
Heiko Schocher <hs@denx.de>
2018-07-13 12:39:23 +0200

    tc_workfd_check_cmd_success.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_check_cmd_success.py
+++ src/tc/linux/tc_workfd_check_cmd_success.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_check_cmd_success.py
 # simple check if previous shell command was succesful
 # End:
 
