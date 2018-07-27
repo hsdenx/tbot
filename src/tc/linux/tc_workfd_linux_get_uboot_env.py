2300556aa1625ee4439594d833cf81981ff94dc1
Heiko Schocher <hs@denx.de>
2018-07-27 07:55:50 +0200

    tc_workfd_linux_get_uboot_env.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_linux_get_uboot_env.py
+++ src/tc/linux/tc_workfd_linux_get_uboot_env.py
@@ -1,17 +1,27 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# tbot.py -s lab_denx -c boardname -t tc_workfd_linux_get_uboot_env.py
 # read U-Boot Environment variable from tb.config.linux_get_uboot_env_name
 # from linux with fw_printenv, and save the value in tb.config.linux_get_uboot_env_value
+#
+# used variables
+#
+# - tb.config.linux_get_uboot_env_name
+#| U-Boot Envvariable, which get read
+#| default: ''
+#
+# - tb.config.linux_get_uboot_env_value
+#| get set from testcase tc_workfd_linux_get_uboot_env.py
+#| default:
+#
 # End:
 
 from tbotlib import tbot
 
+tb.define_variable('linux_get_uboot_env_name', '')
 tb.set_board_state("linux")
 
-logging.info("args: workfd %s %s", tb.workfd.name, tb.config.linux_get_uboot_env_name)
+logging.info("args: workfd %s", tb.workfd.name)
 
 tb.config.linux_get_uboot_env_value = 'undef'
 cmd = 'fw_printenv ' + tb.config.linux_get_uboot_env_name
