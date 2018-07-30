ea67748d9355ce339991ba4c6b9bd783aff4a08e
Heiko Schocher <hs@denx.de>
2018-07-30 07:06:48 +0200

    tc_workfd_rm_linux_code.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_rm_linux_code.py
+++ src/tc/linux/tc_workfd_rm_linux_code.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_rm_linux_code.py
 # rm linux source tb.config.tc_lab_source_dir + '/linux-' + tb.config.boardlabname
 # End:
 
