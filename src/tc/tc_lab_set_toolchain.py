b453362e1285515a1b45740c6418f6851f72fb95
Heiko Schocher <hs@denx.de>
2018-07-30 08:13:54 +0200

    tc_lab_set_toolchain.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/tc_lab_set_toolchain.py
+++ src/tc/tc_lab_set_toolchain.py
@@ -1,13 +1,25 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_lab_set_toolchain.py
-# set the toolchain
+# set eldk toolchain with eldk-switch
+# works only on tb.c_ctrl (change this)
+#
+# used variables
+#
+# - tb.config.tc_lab_toolchain_rev
+#| toolchain revision
+#| default: '5.4'
+#
+# - tb.config.tc_lab_toolchain_name
+#| toolchain name
+#| default: 'armv5te'
+#
 # End:
 
 from tbotlib import tbot
 
+tb.define_variable('tc_lab_toolchain_rev', '5.4')
+tb.define_variable('tc_lab_toolchain_name', 'armv5te')
 c = tb.c_ctrl
 tmp = "eldk-switch -m -r " + tb.config.tc_lab_toolchain_rev + " " + tb.config.tc_lab_toolchain_name
 tb.event.create_event('main', tb.config.boardname, "Toolchain", tmp)
