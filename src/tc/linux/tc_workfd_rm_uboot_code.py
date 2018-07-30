8756873b18197c07e0bfaf424f607641c082c76d
Heiko Schocher <hs@denx.de>
2018-07-30 07:07:34 +0200

    tc_workfd_rm_uboot_code.py: rework testcase documentation
    rework testcase documentation.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/linux/tc_workfd_rm_uboot_code.py
+++ src/tc/linux/tc_workfd_rm_uboot_code.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Description:
-# start with
-# python2.7 src/common/tbot.py -s labconfigname -c boardconfigname -t tc_workfd_rm_uboot_code.py
 # rm U-Boot source tb.config.tc_lab_source_dir + '/u-boot-' + tb.config.boardlabname
 # End:
 
