9c9a399061363d155cb5b1da1af873ecb5a6d2cd
Heiko Schocher <hs@denx.de>
2018-09-02 04:16:25 +0000

    tc_def_lx.py: default value is empty
    set empty default value.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/default/tc_def_lx.py
+++ src/tc/default/tc_def_lx.py
@@ -40,7 +40,7 @@ except:
     logging.info("Setting linux defaults now")
     tb.define_variable('tc_lx_mount_dir', '/home/hs/mnt')
     tb.define_variable('tc_workfd_lx_get_bc_file', '/sys/devices/soc0/soc/2100000.aips-bus/21a0000.i2c/i2c-0/0-0008/bootcount')
-    tb.define_variable('i2c_pre', '')
+    tb.define_variable('i2c_pre', 'empty')
 
 
 tb.config.tc_def_lx_set = 'yes'
