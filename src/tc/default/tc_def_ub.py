b5ec13ed5743bdb17818809d4e2deeab65b9564f
Heiko Schocher <hs@denx.de>
2018-09-07 16:23:49 +0000

    tc_def_ub.py: fix some variables
    fix some wrong converted variables.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/tc/default/tc_def_ub.py
+++ src/tc/default/tc_def_ub.py
@@ -13,6 +13,10 @@
 #| file name for the tftp command
 #| default: ''
 #
+# - tb.config.ub_load_board_env_subdir
+#| subdirectory, where tbot finds boards u-boot env
+#| default: 'tbot'
+#
 # - tb.config.tc_ub_tftp_path
 #| tftp boot directory path for tftp U-Boot command
 #| default: tb.config.tftpboardname + '/' + tb.config.ub_load_board_env_subdir
@@ -26,6 +30,7 @@ try:
     tb.config.tc_def_ub_set
 except:
     logging.info("Setting U-Boot defaults now")
+    tb.define_variable('ub_load_board_env_subdir', 'tbot')
     tb.define_variable('tc_ub_tftp_path', tb.config.tftpboardname + '/' + tb.config.ub_load_board_env_subdir)
     tb.define_variable('tc_ub_tftp_file_addr', tb.config.ub_load_board_env_addr)
     tb.define_variable('tc_ub_tftp_file_name', tb.config.tc_ub_tftp_path + '/env.txt')
